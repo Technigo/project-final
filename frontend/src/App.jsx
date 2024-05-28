1b06d38bddea3d77b8ce0d21ff24f99a457d4b4a
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-05-28 14:30:40 +0200

    changed outcommented lines in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import { PlanetRoutes } from "./routes/PlanetRoutes";
+import { Calculator } from "./components/Calculator";
+
 
 export const App = () => {
   return (
     <>
+      <Calculator/>
       <BrowserRouter>
       <PlanetRoutes />
       </BrowserRouter>
