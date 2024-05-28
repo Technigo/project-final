5e3bf4ee50655c28dd2da757e3e90be7caef721b
unknown <linda.frischknecht@bluewin.ch>
2024-05-28 13:58:37 +0200

    spliting up server file

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,11 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import expressListEndpoints from 'express-list-endpoints'
-
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/flowershop'
-mongoose.connect(mongoUrl)
-mongoose.Promise = Promise
+import app from "./app";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
-
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
-
-// Start defining your routes here
-// http://localhost:8080/
-app.get('/', (req, res) => {
-  const endpoints = expressListEndpoints(app)
-  res.status(200).json(endpoints)
-})
+const port = process.env.PORT || 8080;
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
