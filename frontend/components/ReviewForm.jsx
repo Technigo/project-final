c2b7483defee141358829a9ed23b6e32b41689b4
Martin Jönsson <martin.t.joensson@gmail.com>
2024-05-28 16:52:29 +0200

    Added Shopping Cart route

--- frontend/components/ReviewForm.jsx
+++ frontend/components/ReviewForm.jsx
@@ -1,3 +1,5 @@
+//User needs to be logged in to able to access the review form.
+
 export const ReviewForm = () => {
   return <div>ReviewForm</div>;
 };
--- frontend/src/routes/GlimRoutes.jsx
+++ frontend/src/routes/GlimRoutes.jsx
@@ -5,6 +5,7 @@ import { ProfilePage } from "../pages/ProfilePage";
 import { ProductsPage } from "../pages/ProductsPage";
 import { SingleProductPage } from "../pages/SingleProductPage";
 import { NotFound } from "../pages/NotFound";
+import { ShoppingCart } from "../pages/ShoppingCart";
 
 export const GlimRoutes = () => {
   return (
@@ -12,6 +13,7 @@ export const GlimRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<AboutUs />} />
       <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/cart" element={<ShoppingCart />} />
       <Route path="/products/:id" element={<SingleProductPage />} />
       <Route path="/products" element={<ProductsPage />} />
       <Route path="/*" element={<NotFound />} />
