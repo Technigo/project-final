0b4b8ce8f58d1c4068de836ccd9774f7e5989f0c
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-05-28 21:13:49 +0200

    added endpoints for planets and planet search. Seeded json to database

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import expressListEndpoints from "express-list-endpoints"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import expressListEndpoints from "express-list-endpoints";
+import PlanetModel from "./models/PlanetModel";
 
 //.env
-dotenv.config()
+dotenv.config();
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-solar-system"
-mongoose.connect(mongoUrl)
-mongoose.Promise = Promise
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/project-solar-system";
+mongoose.connect(mongoUrl);
+mongoose.Promise = Promise;
 
 // Defines the port the app will run on.
 const port = process.env.PORT || 8080;
@@ -21,10 +23,39 @@ app.use(express.json());
 
 // Route handler
 app.get("/", (req, res) => {
-  const endpoints = expressListEndpoints(app)
-  res.json(endpoints)
-})
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints);
+});
 
+// GET planets
+app.get("/planets", async (req, res) => {
+  const allPlanets = await PlanetModel.find();
+
+  if (allPlanets.length > 0) {
+    res.json(allPlanets);
+  } else {
+    res.status(404).send("No planets can be found at the moment.");
+  }
+});
+
+// GET a specific planet
+app.get("/planets/:planet", async (req, res) => {
+  const planet = req.params.planet;
+
+  const onePlanet = await PlanetModel.find({
+    name: { $regex: planet, $options: "i" },
+  }).exec();
+
+  if (onePlanet) {
+    res.json(onePlanet);
+  } else {
+    res
+      .status(404)
+      .send(
+        "Couldn't find the specific planet you're looking for. Please try again"
+      );
+  }
+});
 
 // Start the server
 app.listen(port, () => {
