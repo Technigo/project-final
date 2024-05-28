520dd717649fe5fce8b653b5864809690beea9d3
Martin Jönsson <martin.t.joensson@gmail.com>
2024-05-28 14:43:48 +0200

    Routes set up, added NotFound page

--- frontend/src/routes/GlimRoutes.jsx
+++ frontend/src/routes/GlimRoutes.jsx
@@ -1,5 +1,22 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { AboutUs } from "../pages/AboutUs";
+import { ProfilePage } from "../pages/ProfilePage";
+import { ProductsPage } from "../pages/ProductsPage";
+import { SingleProductPage } from "../pages/SingleProductPage";
+import { NotFound } from "../pages/NotFound";
 
 export const GlimRoutes = () => {
-  return <Routes>{/* Add the routes */}</Routes>
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<AboutUs />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/products/:id" element={<SingleProductPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/*" element={<NotFound />} />
+
+      {/* Add the routes */}
+    </Routes>
+  );
+};
