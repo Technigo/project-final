595d309796c8576912843cca57ec5092eb3da74c
Lovisa <lovisa.aaberg@gmail.com>
2024-05-28 12:37:47 +0200

    Added expresslistendpoint

--- backend/server.js
+++ backend/server.js
@@ -8,21 +8,19 @@ mongoose.Promise = Promise
 
 
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// Defines the port the app will run on.
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-// http://localhost:8080/
+// Route handler
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+  const endpoints = expressListEndpoints(app)
+  res.json(endpoints)
+})
 
 
 // Start the server
