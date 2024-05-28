df0383a245f0f6cd44a0b403d9ddbb5031ee25fd
Lovisa <lovisa.aaberg@gmail.com>
2024-05-28 21:44:29 +0200

    Small changes after merge

--- backend/server.js
+++ backend/server.js
@@ -1,75 +1,69 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import expressListEndpoints from "express-list-endpoints";
-import PlanetModel from "./models/PlanetModel";
-import SpaceFeed from "./models/SpaceFeedModel";
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import expressListEndpoints from "express-list-endpoints"
+import PlanetModel from "./models/PlanetModel"
+import SpaceFeed from "./models/SpaceFeedModel"
 
-//.env
-dotenv.config();
+//Load environment variables
+dotenv.config()
 
 const mongoUrl =
-process.env.MONGO_URL || "mongodb://localhost/project-solar-system";
-mongoose.connect(mongoUrl);
-mongoose.Promise = Promise;
+  process.env.MONGO_URL || "mongodb://localhost/project-solar-system"
+mongoose.connect(mongoUrl)
+mongoose.Promise = Promise
 
 // Defines the port the app will run on.
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
 
 // Middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Route handler for the root
 app.get("/", (req, res) => {
-  const endpoints = expressListEndpoints(app);
-  res.json(endpoints);
-});
-
+  const endpoints = expressListEndpoints(app)
+  res.json(endpoints)
+})
 
 // GET planets
 app.get("/planets", async (req, res) => {
-  const allPlanets = await PlanetModel.find();
+  const allPlanets = await PlanetModel.find()
 
   if (allPlanets.length > 0) {
-    res.json(allPlanets);
+    res.json(allPlanets)
   } else {
-    res.status(404).send("No planets can be found at the moment.");
+    res.status(404).send("No planets can be found at the moment.")
   }
-});
+})
 
 // GET a specific planet
 app.get("/planets/:planet", async (req, res) => {
-  const planet = req.params.planet;
+  const planet = req.params.planet
 
   const onePlanet = await PlanetModel.find({
     name: { $regex: planet, $options: "i" },
-  }).exec();
+  }).exec()
 
   if (onePlanet) {
-    res.json(onePlanet);
+    res.json(onePlanet)
   } else {
     res
       .status(404)
       .send(
         "Couldn't find the specific planet you're looking for. Please try again"
-      );
+      )
   }
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+})
 
 //Route handler to show space messages in descending order from when created, limit to 10 messages
 app.get("/space-feed", async (req, res) => {
   const spaceFeed = await SpaceFeed.find()
-  .sort({ createdAt: "desc" })
-  .limit(10)
-  .exec()
+    .sort({ createdAt: "desc" })
+    .limit(10)
+    .exec()
 
   try {
     res.status(201).json({
@@ -137,4 +131,3 @@ app.post("/space-feed/:messageId/like", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
-
