b0f158e885619ff7a85cb8fd3e5410f8a1ff53eb
Paulajungaker <paula010514@hotmail.com>
2024-05-29 13:19:47 +0200

    Adding Header component and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
 import Navbar from "./components/Navbar";
+import Header from "./sections/homePage/Header";
 
 export const App = () => {
-  return <Navbar />;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+    </div>
+  );
 };
