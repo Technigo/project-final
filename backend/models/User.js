942f21e01eb7d3e954387718cc804a5b0476b8a6
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-05-29 13:55:23 +0200

    Created delete endpoint on users/:id Co-authored-by: Sofie Ferrari Strahl <SofieFerrari@users.noreply.github.com>

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,20 @@
-import mongoose from 'mongoose'
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
 
-const { Schema, model } = mongoose
+const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-	username: { type: String, unique: true, required: true },
-	email: {
-		type: String,
-		unique: true,
-		required: true,
-		match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-	},
-	password: { type: String, required: true },
-	accessToken: { type: String, default: () => bcrypt.genSaltSync() },
-})
+  username: { type: String, unique: true, required: true },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  },
+  password: { type: String, required: true },
+  accessToken: { type: String, default: () => bcrypt.genSaltSync() },
+});
 
-const User = model('Thought', userSchema)
+const User = model("User", userSchema);
 
-export default User
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,95 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import User from "./models/User"
+import bcrypt from "bcrypt";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
 
-dotenv.config()
+import User from "./models/User";
 
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-final'
-mongoose.connect(mongoUrl)
-mongoose.Promise = Promise
+dotenv.config();
 
-const port = process.env.PORT || 8080
-const app = express()
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-final";
+mongoose.connect(mongoUrl);
+mongoose.Promise = Promise;
+
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-	res.send('Hello Technigo!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Technigo!");
+});
 
 //route for signing up user
-app.post('/users/signup', async (req, res) => {
-
-})
+app.post("/users/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const user = new User({
+      username,
+      email,
+      password: bcrypt.hashSync(password, 10),
+    });
+    await user.save();
+    res.status(201).json({ message: "Sign up successfully", success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({
+      message: "Could not sign up.",
+      success: false,
+      error: error.message,
+    });
+  }
+});
 
 //route for login user
-app.post('/users/login', async (req, res) => {
-	
-})
+app.post("/users/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        message: "Login Successful.",
+        success: true,
+        accessToken: user.accessToken,
+      });
+    } else if (user) {
+      res.status(401).json({
+        message: "Could not login.",
+        success: false,
+        error: "Incorrect password",
+      });
+    } else {
+      res.status(401).json({
+        message: "Could not login.",
+        success: false,
+        error: "Invalid username",
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: "Could not login. Something is wrong.",
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//route for display user page
+app.post("/users/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+  } catch (error) {}
+});
+
+//route for delete user account
+app.delete("/users/:id", async (req, res) => {});
 
 // Start the server
 app.listen(port, () => {
-	console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
