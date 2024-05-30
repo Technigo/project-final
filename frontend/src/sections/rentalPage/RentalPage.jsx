b390acea32d2eb43cc86792f90a79723c8f714bc
Paulajungaker <paula010514@hotmail.com>
2024-05-30 18:57:15 +0200

    Styling GotoCart section

--- frontend/src/sections/rentalPage/RentalPage.jsx
+++ frontend/src/sections/rentalPage/RentalPage.jsx
@@ -1,8 +1,14 @@
 import RentalList from "./RentalList";
+import GoToCart from "./GoToCart";
 import "../../styling/sectionsStyling/rentalPage/RentalPage.css";
 
 const RentalPage = () => {
-  return <RentalList />;
+  return (
+    <div>
+      <RentalList />
+      <GoToCart />
+    </div>
+  );
 };
 
 export default RentalPage;
