929b1dda87d6ed72159e00bfee09bc358cf337cd
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-05-30 20:34:29 +0200

    updated details in json

--- frontend/src/components/FavoriteFunction.jsx
+++ frontend/src/components/FavoriteFunction.jsx
@@ -1,9 +1,14 @@
-import { IoMdHeartEmpty } from "react-icons/io"
+import { IoMdHeartEmpty, IoMdHeart } from "react-icons/io"
+
+//In this component, user should be able to click on the icon
+//If the user is logged in, the museum should be saved to the users profile and the heart should be filled to indicate museum is saved
+//If user is not logged in, they should be redirected to register/login page
 
 export const FavoriteFunction = () => {
   return (
     <div>
       <IoMdHeartEmpty />
+      <IoMdHeart />
     </div>
   )
 }
