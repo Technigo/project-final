27d8a623bfbf626a5c2c40601c983156d5ecfe1f
Paulajungaker <paula010514@hotmail.com>
2024-05-30 15:02:35 +0200

    Styling the login component

--- frontend/src/sections/loginPage/LoginPage.jsx
+++ frontend/src/sections/loginPage/LoginPage.jsx
@@ -1,19 +1,23 @@
+import "../../styling/sectionsStyling/authPages/AuthPages.css";
+
 const LoginPage = () => {
   return (
-    <div className="loginContainer">
-      <h1 className="loginTitle">Sign in</h1>
-      <form className="loginForm">
-        <label className="loginLabel">
+    <div className="authContainer">
+      <form className="authForm">
+        <h2 className="authTitle">Sign in</h2>
+        <label className="authLabel">
           Email:
           <input type="email" required className="emailInput" />
         </label>
-        <label className="loginLabel">
+        <label className="authLabel">
           Password:
           <input type="password" required className="passwordInput" />
         </label>
-        <button type="submit" className="loginFormButton">
-          Log In
-        </button>
+        <div className="authButtonWrapper">
+          <button type="submit" className="authButton">
+            Log In
+          </button>
+        </div>
       </form>
     </div>
   );
