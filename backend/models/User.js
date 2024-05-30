9f7494f12d2ec3af815f67cf23b747d8360d582d
Maria-source-cell <manuelabirnat@gmail.com>
2024-05-30 10:50:35 +0200

    backend

---
+++ backend/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, minlength: 3 },
+  password: { type: String, required: true, minlength: 6 },
+  role: { type: String, enum: ["Listener", "Seeker"], required: true },
+  name: { type: String },
+  bio: { type: String },
+  profilePicture: { type: String },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,95 @@
 import express from "express";
 import cors from "cors";
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import passport from "./config/passport";
+import User from "./models/User";
+import expressListEndpoints from "express-list-endpoints";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/flowershop"
-mongoose.connect(mongoUrl)
-mongoose.Promise = Promise
+dotenv.config();
 
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-final";
+mongoose.connect(mongoUrl);
+mongoose.Promise = global.Promise;
 
+// Function to generate JWT access token
+const generateAccessToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+};
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
+// Middleware to athenticate the token
+const authenticateToken = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    return res.status(401).json({ error: "Token is missing" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Token is invalid" });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+const port = process.env.PORT || 9000;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
 
 // Start defining your routes here
-// http://localhost:8080/
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints);
 });
 
+app.post("/users", async (req, res) => {
+  const { username, password, role } = req.body;
+  try {
+    const newUser = new User({ username, password, role });
+    await newUser.save();
+    const accessToken = generateAccessToken(newUser._id);
+    res.status(201).json({ id: newUser._id, accessToken });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    if (error.code === 11000) {
+      res.status(400).json({ error: "Username already exists" });
+    } else {
+      res.status(500).json({ error: "Something went wrong" });
+    }
+  }
+});
+
+app.post("/sessions", (req, res, next) => {
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res
+        .status(400)
+        .json({ error: info ? info.message : "Login failed" });
+    }
+    req.login(user, { session: false }, async (err) => {
+      if (err) {
+        return res.send(err);
+      }
+      const token = generateAccessToken(user._id);
+      return res.json({ user, token });
+    });
+  })(req, res, next);
+});
+
+// Authenticated endpoint
+app.get("/secrets", authenticateToken, (req, res) => {
+  res.json({ secret: "This is secret content" });
+});
 
 // Start the server
 app.listen(port, () => {
