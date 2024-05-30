c2f09a0d9ebfd1eeb523716dca5baff7cf566b39
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-05-30 15:35:23 +0200

    commit

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -4,7 +4,12 @@ export const NotFoundPage = () => {
   return (
     <div>
       <ToHomepageBtn />
-      <h4>404 not found</h4>
+      <h4>404: Artifact Not Found</h4>
+      <p>
+        Oh no! You've uncovered a missing exhibit. This page is as elusive as a
+        hidden treasure. But don't worry, there are hundreds of museum to
+        explore!
+      </p>
     </div>
   )
 }
