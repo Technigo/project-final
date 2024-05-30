a16dffd1b8fea182616a4759e5f72136e9dabc6f
Paulajungaker <paula010514@hotmail.com>
2024-05-30 02:04:33 +0200

    Creating Login Component

--- frontend/src/sections/loginPage/LoginPage.jsx
+++ frontend/src/sections/loginPage/LoginPage.jsx
@@ -1,5 +1,22 @@
 const LoginPage = () => {
-  return;
+  return (
+    <div className="loginContainer">
+      <h1 className="loginTitle">Sign in</h1>
+      <form className="loginForm">
+        <label className="loginLabel">
+          Email:
+          <input type="email" required className="emailInput" />
+        </label>
+        <label className="loginLabel">
+          Password:
+          <input type="password" required className="passwordInput" />
+        </label>
+        <button type="submit" className="loginFormButton">
+          Log In
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
