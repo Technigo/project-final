da6069b0db7d00e7764aed7ad0a75b289d57184a
Martin Jönsson <martin.t.joensson@gmail.com>
2024-05-30 16:27:36 +0200

    navbar tablet

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -14,20 +14,24 @@ export const Navigation = () => {
 
   const showNavbar = () => {
     navRef.current.classList.toggle("hidden");
-    btnRef.current.classList.toggle("hidden");
+    btnRef.current.classList.toggle("invisible");
   };
 
   return (
     <nav className="grid grid-cols-3 bg-main-red justify-between">
       <div className="left-nav flex">
-        <NavLink to="/profile" className="text-white">
-          <img src={userIcon} alt="Profile" className="h-4 m-2 laptop:hidden" />
-          <p className="font-body text-white font-extrabold text-lg hidden laptop:block">
+        <NavLink to="/profile" className="text-white m-2">
+          <img src={userIcon} alt="Profile" className="h-4 tablet:hidden" />
+          <p className="font-body text-white font-extrabold text-lg hidden tablet:block">
             Sign In
           </p>
         </NavLink>
-        <NavLink to="/cart" className="text-white">
-          <img src={shoppingCart} alt="Shopping cart" className="h-4 m-2" />
+        <NavLink to="/cart" className="text-white tablet:hidden">
+          <img
+            src={shoppingCart}
+            alt="Shopping cart"
+            className="h-4 m-2 tablet:h-6"
+          />
         </NavLink>
       </div>
       <div className="center-nav flex m-auto">
@@ -35,40 +39,42 @@ export const Navigation = () => {
           <img className="w-40 my-4" src={glimLogo} alt="glim logo" />
         </NavLink>
       </div>
-      <div className="right-nav flex flex-row-reverse">
+      <div className="right-nav flex flex-row justify-end">
         <NavLink to="/cart" className="text-white">
-          <img src={shoppingCart} alt="Shopping cart" className="w-6 hidden" />
+          <img
+            src={shoppingCart}
+            alt="Shopping cart"
+            className="h-6 m-2 hidden tablet:block"
+          />
         </NavLink>
 
-        <NavLink to="/products" className="text-white hidden">
-          <p>Products</p>
-        </NavLink>
-
-        <div>
-          <div
-            className="hidden absolute top-0 right-0 flex-col text-right p-4 rounded-bl-lg text-white bg-strong-red"
-            ref={navRef}
-          >
-            <button className="" onClick={showNavbar}>
-              <img src={xMark} alt="Menu" className="h-4" />
-            </button>
-            <NavLink className="nav-link" to="/now_playing">
-              <p className="my-4">Profile</p>
-            </NavLink>
-            <NavLink className="nav-link" to="/products">
-              <p>Products</p>
-            </NavLink>
-            <NavLink className="nav-link" to="/about">
-              <p>About us</p>
-            </NavLink>
-            <NavLink className="nav-link" to="/cart">
-              <p className="my-4">Your cart</p>
-            </NavLink>
-          </div>
-          <button ref={btnRef} className="" onClick={showNavbar}>
-            <img src={burgerMenu} alt="Menu" className="h-4 m-2" />
+        <div
+          className="hidden absolute top-0 right-0 flex-col text-right  p-4 rounded-bl-lg text-white bg-strong-red"
+          ref={navRef}
+        >
+          <button className="" onClick={showNavbar}>
+            <img src={xMark} alt="Menu" className="h-4" />
           </button>
+          <NavLink className="nav-link" to="/now_playing">
+            <p className="my-4">Profile</p>
+          </NavLink>
+          <NavLink className="nav-link" to="/products">
+            <p>Products</p>
+          </NavLink>
+          <NavLink className="nav-link" to="/about">
+            <p>About us</p>
+          </NavLink>
+          <NavLink className="nav-link" to="/cart">
+            <p className="my-4">Your cart</p>
+          </NavLink>
         </div>
+        <button ref={btnRef} className="flex" onClick={showNavbar}>
+          <img
+            src={burgerMenu}
+            alt="Menu"
+            className="h-4 m-2 justify-start tablet:h-6"
+          />
+        </button>
       </div>
     </nav>
   );
