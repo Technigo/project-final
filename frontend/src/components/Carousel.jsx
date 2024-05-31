c4fae0bc7c63721e84992b23b28270cb769ccc59
Martin Jönsson <martin.t.joensson@gmail.com>
2024-05-31 16:38:38 +0200

    trying to add draggable carousel

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,26 +1,71 @@
+import { useEffect, useRef } from "react";
 import { ProductCard } from "./ProductCard";
 
 export const Carousel = () => {
+  const sliderRef = useRef(null);
+  let mouseDown = false;
+  let startX, scrollLeft;
+
+  const startDragging = (e) => {
+    mouseDown = true;
+    startX = e.pageX - sliderRef.current.offsetLeft;
+    scrollLeft = sliderRef.current.scrollLeft;
+  };
+
+  const stopDragging = (e) => {
+    mouseDown = false;
+  };
+
+  const move = (e) => {
+    e.preventDefault();
+    if (!mouseDown) {
+      return;
+    }
+    const x = e.pageX - sliderRef.current.offsetLeft;
+    const scroll = x - startX;
+    sliderRef.current.scrollLeft = scrollLeft - scroll;
+  };
+
+  // Add the event listeners
+  useEffect(() => {
+    const slider = sliderRef.current;
+    if (slider) {
+      slider.addEventListener("mousemove", move, false);
+      slider.addEventListener("mousedown", startDragging, false);
+      slider.addEventListener("mouseup", stopDragging, false);
+      slider.addEventListener("mouseleave", stopDragging, false);
+
+      return () => {
+        slider.addEventListener("mousemove", move, false);
+        slider.addEventListener("mousedown", startDragging, false);
+        slider.addEventListener("mouseup", stopDragging, false);
+        slider.addEventListener("mouseleave", stopDragging, false);
+      };
+    }
+  }, []);
+
   return (
     <div>
-      Carousel
-      <div className="whitespace-nowrap px-[50%] scroll-p-4 snap-x snap-mandatory overflow-auto p-2 flex gap-2">
-        <div className="snap-center">
+      <div
+        ref={sliderRef}
+        className=" whitespace-nowrap scroll-auto px-[50%] scroll-p-4 snap-x snap-mandatory overflow-auto p-2 flex gap-2"
+      >
+        <div className="child snap-center ">
           <ProductCard />
         </div>
-        <div className="snap-center">
+        <div className="child snap-center">
           <ProductCard />
         </div>
-        <div className="snap-center">
+        <div className="child snap-center">
           <ProductCard />
         </div>
-        <div className="snap-center">
+        <div className="child snap-center">
           <ProductCard />
         </div>
-        <div className="snap-center">
+        <div className="child snap-center">
           <ProductCard />
         </div>
-        <div className="snap-center">
+        <div className="child snap-center">
           <ProductCard />
         </div>
       </div>
