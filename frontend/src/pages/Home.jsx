56b2973bc2aecc597a722889980965017634c5cf
Martin Jönsson <martin.t.joensson@gmail.com>
2024-05-31 11:56:50 +0200

    Review card, only placeholder text for now.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { ProductCard } from "../components/ProductCard";
+import { ReviewCard } from "../components/ReviewCard";
 
 export const Home = () => {
   return (
     <div>
       Home
       <ProductCard />
+      <ReviewCard />
     </div>
   );
 };
