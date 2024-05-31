bc4b7334ddef34e129efdedf84edf46184f2347e
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-05-31 14:50:11 +0200

    Add route and controller for liking or disliking a product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,12 +2,26 @@ import { Product } from "../models/Product";
 import asyncHandler from "express-async-handler";
 
 export const displayProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find().select("-__v").exec();
+  const products = await Product.find().select("-__v -description").exec();
   res.status(200).json(products);
 });
 
 export const displayProductById = asyncHandler(async (req, res) => {
   const { productId } = req.params;
-  const product = await Product.findOne({ _id: productId }).exec();
+  const product = await Product.findOne({ _id: productId }, "-__v").exec();
+  res.status(200).json(product);
+});
+
+export const likeOrUnlikeProductById = asyncHandler(async (req, res) => {
+  const { productId } = req.params;
+  const { unlike } = req.body;
+  const product = await Product.findOne({ _id: productId });
+  if (unlike) {
+    product.likes--;
+    await product.save();
+  } else {
+    product.likes++;
+    await product.save();
+  }
   res.status(200).json(product);
 });
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   displayProducts,
   displayProductById,
+  likeOrUnlikeProductById,
 } from "../controllers/productController";
 
 const router = express.Router();
@@ -9,5 +10,7 @@ const router = express.Router();
 router.get("/products", displayProducts);
 
 router.get("/products/:productId", displayProductById);
+
+router.patch("/products/:productId", likeOrUnlikeProductById);
 
 export default router;
