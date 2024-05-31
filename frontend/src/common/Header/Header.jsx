fdbbde43f75f1662123a2018aa321f1ae2829141
Frida Svensson <fridamarisvensson@gmail.com>
2024-05-31 12:46:43 +0200

    Import logo and icons to assets folder and add them to the header

--- frontend/src/common/Header/Header.jsx
+++ frontend/src/common/Header/Header.jsx
@@ -1,14 +1,18 @@
 import "./Header.css";
 import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/logotypes/logo-stamp.svg";
+import cart from "../../assets/icons/cart.svg";
+import search from "../../assets/icons/search.svg";
+import user from "../../assets/icons/user.svg";
 
 export const Header = () => {
   return (
     <header>
       <nav>
-        <h1>
-          <Link to="/">Logo</Link>
-        </h1>
-        <ul>
+        <Link to="/">
+          <img src={logo} className="logo-stamp" alt="logo"></img>
+        </Link>
+        <ul className="navbar-categories">
           <li>
             <NavLink to="/products">All products</NavLink>
           </li>
@@ -24,6 +28,17 @@ export const Header = () => {
           <li>
             <NavLink to="/products/category/:category">Accessories</NavLink>
           </li>
+        </ul>
+        <ul className="navbar-icons">
+          <button>
+            <img src={search} alt="search-icon"></img>
+          </button>
+          <Link to="/">
+            <img src={user} alt="my-account"></img>
+          </Link>
+          <Link to="/">
+            <img src={cart} alt="shopping-cart"></img>
+          </Link>
         </ul>
       </nav>
     </header>
