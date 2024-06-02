35f36d00c4d3547345ffb8675c51433481a26ede
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-06-02 20:30:24 +0200

    added map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
-export const App = () => {
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
+export const App = () => {
   return (
     <>
-      <h1>Welcome to Final Project!</h1>
+      <h1>happy angry note</h1>
+      <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+        <Map
+          style={{ width: "100vw", height: "100vh" }}
+          defaultCenter={{ lat: 22.54992, lng: 0 }}
+          defaultZoom={3}
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+        />
+      </APIProvider>
     </>
   );
 };
