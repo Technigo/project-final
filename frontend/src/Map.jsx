6187fe13a76096bf5205facabb95726f97e98643
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-06-02 20:43:46 +0200

    added css

--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -1,12 +1,13 @@
 import { APIProvider, Map as ReactGoogleMap } from "@vis.gl/react-google-maps";
+import "./Map.css";
 
 export const Map = () => {
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <ReactGoogleMap
-        style={{ width: "100vw", height: "100vh" }}
-        defaultCenter={{ lat: 22.54992, lng: 0 }}
-        defaultZoom={3}
+        className="Map"
+        defaultCenter={{ lat: 62.3875, lng: 16.325556 }}
+        defaultZoom={5}
         gestureHandling={"greedy"}
         disableDefaultUI={true}
       />
