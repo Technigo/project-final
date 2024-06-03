bdfe7a4defb310dd7d267c3e3f89ff2b5ab89cd1
Erica Mechler <erica.mechler1@gmail.com>
2024-06-03 12:15:45 +0200

    added button-component and implemented in hero component, also added some styling to hero-component

--- frontend/src/pages/Home/components/Hero/Hero.jsx
+++ frontend/src/pages/Home/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import "./Hero.css";
 import heroImage from "/assets/images/hero-image.jpg";
 import { Image } from "../../../../common/ReusableComponents/Image";
 import { Link } from "react-router-dom";
+import { Button } from "../../../../common/ReusableComponents/Button";
 
 export const Hero = () => {
   return (
@@ -11,12 +12,10 @@ export const Hero = () => {
         alt="Hero-image of small girl dancing in the field"
         className="hero"
       />
+      <h1 className="hero-h1">Summer vibes</h1>
       <div className="hero-text">
-        <h1>Summer vibes</h1>
-        <p>Playful clothes for sunny days</p>
-        <Link to="/products">
-          <button>See all products</button>
-        </Link>
+        <h4 className="hero-h4">Playful clothes for sunny days</h4>
+        <Button label="See all products" className="hero" to="/products" />
       </div>
     </div>
   );
