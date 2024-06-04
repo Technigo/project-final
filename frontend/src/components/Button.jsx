f230d562587bec02386b28e62f3b2227126f14c6
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-06-04 12:48:49 +0200

    Finish button component

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,8 +1,22 @@
+import PropTypes from "prop-types";
 
-const Button = () => {
+export const Button = ({ text, style, onClick }) => {
+  const blueStyle = "bg-button-blue shadow-[0px_4px_4px_#000000] text-white";
+  const whiteStyle =
+    "border-button-blue text-button-blue border-2 border-solid";
+  // font weight is 700 acc. to the design - need to import Poppins with 700 weight
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      onClick={onClick}
+      className={`${style === "white" ? whiteStyle : blueStyle} min-h-12 w-48 rounded px-1.5 font-poppins text-base`}
+    >
+      {text}
+    </button>
+  );
+};
 
-export default Button
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  style: PropTypes.string,
+  onClick: PropTypes.func,
+};
