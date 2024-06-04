2c03cbc548a959ea6a32d48a1d561cdc78e54c91
Frida Svensson <fridamarisvensson@gmail.com>
2024-06-04 11:35:41 +0200

    Successfully fetch product data and display on SPP

--- frontend/src/pages/SingleProduct/SingleProduct.jsx
+++ frontend/src/pages/SingleProduct/SingleProduct.jsx
@@ -1,47 +1,41 @@
 import { Image } from "../../common/ReusableComponents/Image";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "./SingleProduct.css";
+import { DeliveryStatements } from "../Home/components/DeliveryStatements/DeliveryStatements";
 
-export const SingleProduct = (product) => {
+export const SingleProduct = () => {
   const { productId } = useParams(); //get the product ID from the URL
   const [product, setProduct] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    const fetchProduct = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `https://cones-and-stones-ppnudpghiq-lz.a.run.app/products/${productId}`
+        );
+
+        const data = await response.json();
+        if (data.success) {
+          setProduct(data.response);
+          setError(null);
+          setTimeout(() => {
+            setIsLoading(false);
+          }, 3000);
+        } else {
+          setError(data.error.message);
+        }
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
     fetchProduct();
   }, [productId]);
-
-  const fetchProduct = () => {
-    setIsLoading(true); // Set loading to true before fetch starts
-
-    fetch(`https://cones-and-stones-ppnudpghiq-lz.a.run.app/products/${_id}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Could not load product");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log("Fetched data:", data); // Log the fetched data
-        if (data.success && data.response) {
-          setProduct(data.response);
-          setError(null); // Clear error if fetch is successful
-        } else {
-          throw new Error("Fetched data is not in the expected format");
-        }
-
-        // Introduce a delay before setting loading to false
-        setTimeout(() => {
-          setIsLoading(false);
-        }, 100);
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error); // Log the error
-        setError(error.message); // Set error message in state
-        setIsLoading(false); // Set loading to false after fetch completes
-      });
-  };
 
   return (
     <div className="singleproduct-page">
@@ -49,26 +43,27 @@ export const SingleProduct = (product) => {
         {isLoading && <p>Loading...</p>}
         {error && <p>Error: {error}</p>}
       </div>
-      <div className="image-container">
-        <Image src={product.image_url} alt="product-image" />
-      </div>
-      <div className="product-info">
-        <h2>{product.name}</h2>
-        <h4>{product.price}</h4>
-        <p>{product.description}</p>
-        <p>{product.details}</p>
-        <div className="size-buttons">
-          {product.stock.map((item, index) => (
-            <button key={index}>{item.size}</button>
-          ))}
+
+      {product && (
+        <div>
+          <div className="image-container">
+            <Image src={product.image_url} alt="product-image" />
+          </div>
+          <div className="product-info">
+            <h2>{product.name}</h2>
+            <h4>{product.price} SEK</h4>
+            <p>{product.description}</p>
+            <p>{product.details}</p>
+            <div className="size-buttons">
+              {product.stock.map((item, index) => (
+                <button key={index}>{item.size}</button>
+              ))}
+            </div>
+            <button className="add-to-cart-button">Add to cart</button>
+            <DeliveryStatements className="spp-delivery"/>
+          </div>
         </div>
-        <button className="add-to-cart-button">Add to cart</button>
-        <div className="delivery-icons">
-          <img alt="Free shipping" />
-          <img alt="Fast delivery" />
-          <img alt="Pick up in store" />
-        </div>
-      </div>
+      )}
     </div>
   );
 };
