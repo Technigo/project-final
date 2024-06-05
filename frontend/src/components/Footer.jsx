2aae544ccc893780de66967b26993799722bf8eb
SofieFerrari <sofiestrahl@gmail.com>
2024-06-05 12:16:45 +0200

    worked on shop-element

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import dropdown from "../assets/dropdown.svg";
 import { useState } from "react";
 
 export const Footer = () => {
@@ -35,17 +36,19 @@ export const Footer = () => {
   const myAccount = ["MY PAGE", "LOGIN", "LOGOUT"];
 
   return (
-    <footer className="bg-blue pt-5 leading-8 text-white">
+    <footer className="bg-blue pt-5 leading-9 text-white font-bold font-montserrat">
       <div>
-        <button className="ml-3" onClick={toggelShop}>
-          SHOP
+        <button
+          className="ml-3 flex w-60 flex-row border-b justify-between"
+          onClick={toggelShop}
+        >
+          <h4 className="">SHOP</h4>
+          <img src={dropdown} className="h-2 self-center" />
         </button>
         {shopIsOpen && (
           <ul className="ml-6 leading-7">
             {categories.map((category, index) => (
-              <li key={index} className="dropdown-item">
-                {category}
-              </li>
+              <li key={index}>{category}</li>
             ))}
           </ul>
         )}
@@ -57,9 +60,7 @@ export const Footer = () => {
         {accountIsOpen && (
           <ul className="ml-6 leading-7">
             {myAccount.map((account, index) => (
-              <li key={index} className="dropdown-item">
-                {account}
-              </li>
+              <li key={index}>{account}</li>
             ))}
           </ul>
         )}
@@ -76,7 +77,7 @@ export const Footer = () => {
         )}
       </div>
       <div>
-        <button className="ml-3 " onClick={toggleAbout}>
+        <button className="ml-3" onClick={toggleAbout}>
           ABOUT US
         </button>
         {aboutIsOpen && (
@@ -87,10 +88,10 @@ export const Footer = () => {
         )}
       </div>
       <div>
-        <h1 className="py-7 text-center font-semibold">webify</h1>
-        <p className="py-7 text-center font-light">
-          Copyright 2024 © Wen Z & Sofie FS & Mai K Technigo Bootcamp Final
-          Project
+        <h1 className="py-7 text-center font-bold font-poppins">webify</h1>
+        <p className="py-7 text-center text-xs font-lato">
+          Copyright 2024 © <br /> Wen Z & Sofie FS & Mai K <br /> Technigo
+          Bootcamp Final Project
         </p>
       </div>
     </footer>
