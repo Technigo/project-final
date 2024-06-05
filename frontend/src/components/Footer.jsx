7a098b608aca5c8ba9bbd564f27c90db9ee91142
SofieFerrari <sofiestrahl@gmail.com>
2024-06-05 13:14:31 +0200

    completed footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -36,17 +36,17 @@ export const Footer = () => {
   const myAccount = ["MY PAGE", "LOGIN", "LOGOUT"];
 
   return (
-    <footer className="bg-blue pt-5 leading-9 text-white font-bold font-montserrat">
+    <footer className="flex flex-col items-center bg-blue pt-6 font-montserrat leading-9 text-white">
       <div>
         <button
-          className="ml-3 flex w-60 flex-row border-b justify-between"
+          className="ml-3 flex w-[300px] flex-row justify-between border-b"
           onClick={toggelShop}
         >
-          <h4 className="">SHOP</h4>
+          <h4 className="mb-1 mt-2 text-xl font-bold tracking-wider">SHOP</h4>
           <img src={dropdown} className="h-2 self-center" />
         </button>
         {shopIsOpen && (
-          <ul className="ml-6 leading-7">
+          <ul className="ml-6 space-y-2 py-3 text-sm leading-7 tracking-wide">
             {categories.map((category, index) => (
               <li key={index}>{category}</li>
             ))}
@@ -54,11 +54,17 @@ export const Footer = () => {
         )}
       </div>
       <div>
-        <button className="ml-3" onClick={toggelAccount}>
-          MY ACCOUNT
+        <button
+          className="ml-3 flex w-[300px] flex-row justify-between border-b"
+          onClick={toggelAccount}
+        >
+          <h4 className="mb-1 mt-2 text-xl font-bold tracking-wider">
+            MY ACCOUNT
+          </h4>
+          <img src={dropdown} className="h-2 self-center" />
         </button>
         {accountIsOpen && (
-          <ul className="ml-6 leading-7">
+          <ul className="ml-6 space-y-2 py-3 text-sm leading-7 tracking-wide">
             {myAccount.map((account, index) => (
               <li key={index}>{account}</li>
             ))}
@@ -66,32 +72,46 @@ export const Footer = () => {
         )}
       </div>
       <div>
-        <button className="ml-3" onClick={toggelCustomerService}>
-          CUSTOMER SERVICE
+        <button
+          className="ml-3 flex w-[300px] flex-row justify-between border-b"
+          onClick={toggelCustomerService}
+        >
+          <h4 className="mb-1 mt-2 text-xl font-bold tracking-wider">
+            CUSTOMER SERVICE
+          </h4>
+          <img src={dropdown} className="h-2 self-center" />
         </button>
         {customerIsOpen && (
-          <p className="ml-6">
+          <p className="ml-6 py-3 leading-6 tracking-wide w-[280px]">
             This is not a real web shop, but if it was, we'd proably have the
             best customer service in the world!
           </p>
         )}
       </div>
       <div>
-        <button className="ml-3" onClick={toggleAbout}>
-          ABOUT US
+        <button
+          className="ml-3 flex w-[300px] flex-row justify-between border-b"
+          onClick={toggleAbout}
+        >
+          <h4 className="mb-1 mt-2 text-xl font-bold tracking-wider">
+            ABOUT US
+          </h4>
+          <img src={dropdown} className="h-2 self-center" />
         </button>
         {aboutIsOpen && (
-          <p className="ml-6">
+          <p className="leading-6 tracking-wide w-[280px] ml-6 py-3">
             This site was designed and developed by Mai, Sofie and Wen for our
             final project of the Web Development Bootcamp at Technigo
           </p>
         )}
       </div>
       <div>
-        <h1 className="py-7 text-center font-bold font-poppins">webify</h1>
-        <p className="py-7 text-center text-xs font-lato">
-          Copyright 2024 © <br /> Wen Z & Sofie FS & Mai K <br /> Technigo
-          Bootcamp Final Project
+        <h1 className="py-7 text-center font-poppins text-5xl font-bold tracking-wider">
+          webify
+        </h1>
+        <p className="py-7 text-center font-lato text-xs">
+          Copyright 2024 © Wen Z & Sofie FS & Mai K <br /> Technigo Bootcamp
+          Final Project
         </p>
       </div>
     </footer>
