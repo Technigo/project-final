a81ed649d14286b427d12126f96b9ee2a9f18472
Mai Kanetaka <maikanetaka@gmail.com>
2024-06-05 22:50:50 +0200

    add style

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -23,41 +23,44 @@ export const Form = ({ isLogin, onSubmit }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>
-          Username:
+    <form
+      onSubmit={handleSubmit}
+      className="mx-auto max-w-md space-y-6 rounded-lg bg-white px-4 py-8 shadow-md"
+    >
+      <div className="flex flex-col">
+        <label className="text-gray-700 mb-2 font-bold">
           <input
             type="text"
             name="username"
             value={formData.username}
             onChange={handleChange}
+            className="mt-1 p-2 block w-full border-2 border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
             required
           />
         </label>
       </div>
       {!isLogin && (
-        <div>
-          <label>
-            Emai:
+        <div className="flex flex-col">
+          <label className="text-gray-700 mb-2 font-bold">
             <input
               type="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
+              className="mt-1 p-2 block w-full border-2 border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
               required
             />
           </label>
         </div>
       )}
-      <div>
-        <label>
-          Password:
+      <div className="flex flex-col">
+        <label className="text-gray-700 mb-2 font-bold">
           <input
             type="password"
             name="password"
             value={formData.password}
             onChange={handleChange}
+            className="mt-1 p-2 block w-full border-2 border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
             required
           />
         </label>
@@ -65,6 +68,7 @@ export const Form = ({ isLogin, onSubmit }) => {
       <Button
         text={isLogin ? "Log in" : "Sign up"}
         style="blue"
+        type="submit"
         onClick={(event) => {
           event.preventDefault();
           handleSubmit(event);
