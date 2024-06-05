b57e8d526e8766d8edff49ddd3a78852638401b9
SofieFerrari <sofiestrahl@gmail.com>
2024-06-05 13:16:10 +0200

    removed code in app.jsx to avoid merge conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
-import { Footer } from "./components/Footer";
-
 export const App = () => {
 
   return (
     <>
       <h1 className="text-cyan-500 font-montserrat"	>Welcome to webify!!</h1>
-      <Footer />
     </>
   );
 };
