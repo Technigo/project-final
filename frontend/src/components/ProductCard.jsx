c6edaeb89b66f1ed6c57c8389f646ac0315aaea2
izzibizz <contact@izabellind.com>
2024-06-05 17:58:06 +0200

    working on product-list page

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -11,15 +11,16 @@ export const ProductCard = ({ data }) => {
   const addToCart = "Add to Cart"; //productLangData.add-to-cart
 
   return (
-    <div className="bg-strong-red m-auto min-w-[200px]  relative w-2/3 max-w-2/3 rounded-xl">
-      <div className="absolute bg-transparent w-[100%] h-[100%]"></div>
-      <img className="w-100% rounded-t-xl" src={image} alt="" />
-      <div className="m-2 flex flex-col items-center text-white">
-        <h3 className="font-heading">{productName}</h3>
-        <p className="font-body my-2">{price}</p>
-        <button className="w-28 bg-button-light my-2 rounded-full text-text-dark">
+    <div className="bg-strong-red m-auto w-full h-full rounded-xl pb-5">
+      {/* <div className="absolute w-[100%] h-[100%]"></div> */}
+      <img className="w-full rounded-t-xl" src={image} alt="" />
+      <div className="m-4 flex flex-col items-center justify-between text-white">
+        <h3 className="font-heading text-xs laptop:text-sm  ">{productName}</h3>
+        <div className="flex flex-col items-center "><p className="font-body my-3 font-bold text-sm">{price}</p>
+        <button className="w-24 text-xs bg-button-light p-1 rounded-full text-text-dark hover:bg-main-yellow">
           {addToCart}
         </button>
+        </div>
       </div>
     </div>
   );
