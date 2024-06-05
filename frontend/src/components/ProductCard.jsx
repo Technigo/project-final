542b816604b0ba37a88dd01c62a5766853e4cab9
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-06-05 14:50:43 +0200

    Create productCard component

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,39 @@
+import favorite from "../assets/favorite-icon.svg";
+import cart from "../assets/cart-blue.svg";
+import PropTypes from "prop-types";
+
+export const ProductCard = ({ templateImg, tags, name, price }) => {
+  return (
+    <div className="flex flex-col gap-3">
+      <img src={templateImg} alt="image of the template" />
+      <div className="flex flex-row justify-between font-montserrat">
+        <div className="flex flex-col">
+          <span className="mb-2 flex flex-row">
+            {tags.split(", ").map((tag) => (
+              <button key={tag} className="mr-2 text-sm text-blue">
+                #tag
+              </button>
+            ))}
+          </span>
+          <p className="font-montserrat text-xl font-bold">{name}</p>
+          <p className="text-sm">€{price}</p>
+        </div>
+        <div className="mr-3 flex flex-row items-center gap-6">
+          <button className="h-6 w-6">
+            <img src={favorite} alt="add to favorite button" />
+          </button>
+          <button className="h-6 w-6">
+            <img src={cart} alt="add to cart button" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ProductCard.propTypes = {
+  templateImg: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -1,3 +1,44 @@
+import { ProductCard } from "../components/ProductCard";
+
 export const ProductList = () => {
-  return <div>ProductList</div>;
+  return (
+    <div className="lg:mx-[minmax(20px, auto)] mx-10 grid grid-cols-1 justify-center justify-items-center gap-10 lg:grid-cols-[repeat(3,_minmax(0,_500px))] lg:gap-x-[5%]">
+      <ProductCard
+        templateImg="https://res.cloudinary.com/ddpsnaef5/image/upload/v1717358234/cld-sample-5.jpg"
+        tags="Health, Wellness, Holistic"
+        name="Health and Wellness"
+        price={34.99}
+      />
+      <ProductCard
+        templateImg="https://res.cloudinary.com/ddpsnaef5/image/upload/v1717358234/cld-sample-5.jpg"
+        tags="Health, Wellness, Holistic"
+        name="Health and Wellness"
+        price={34.99}
+      />
+      <ProductCard
+        templateImg="https://res.cloudinary.com/ddpsnaef5/image/upload/v1717358234/cld-sample-5.jpg"
+        tags="Health, Wellness, Holistic"
+        name="Health and Wellness"
+        price={34.99}
+      />
+      <ProductCard
+        templateImg="https://res.cloudinary.com/ddpsnaef5/image/upload/v1717358234/cld-sample-5.jpg"
+        tags="Health, Wellness, Holistic"
+        name="Health and Wellness"
+        price={34.99}
+      />
+      <ProductCard
+        templateImg="https://res.cloudinary.com/ddpsnaef5/image/upload/v1717358234/cld-sample-5.jpg"
+        tags="Health, Wellness, Holistic"
+        name="Health and Wellness"
+        price={34.99}
+      />
+      <ProductCard
+        templateImg="https://res.cloudinary.com/ddpsnaef5/image/upload/v1717358234/cld-sample-5.jpg"
+        tags="Health, Wellness, Holistic"
+        name="Health and Wellness"
+        price={34.99}
+      />
+    </div>
+  );
 };
