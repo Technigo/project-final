ef3139506158a8db50f7626fe92b1bea65247b8a
Lovisa <lovisa.aaberg@gmail.com>
2024-06-05 13:19:58 +0200

    Deleted link to home in planets

--- frontend/src/components/Planet.jsx
+++ frontend/src/components/Planet.jsx
@@ -1,57 +1,52 @@
 // parent component for PlanetDetail & PlanetList
 
-import { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
-import { PlanetDetails } from "./PlanetDetails";
-import { PlanetList } from "./PlanetList";
+import { useState, useEffect } from "react"
+import { useParams, Link } from "react-router-dom"
+import { PlanetDetails } from "./PlanetDetails"
+import { PlanetList } from "./PlanetList"
 
 export const Planet = () => {
-  const { planet } = useParams();
-  const [onePlanet, setOnePlanet] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const URL = `https://project-final-45vw.onrender.com/planets/${planet}`;
+  const { planet } = useParams()
+  const [onePlanet, setOnePlanet] = useState({})
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const URL = `https://project-final-45vw.onrender.com/planets/${planet}`
 
   useEffect(() => {
     const fetchOnePlanet = async () => {
-      setLoading(true);
-      setError(null);
+      setLoading(true)
+      setError(null)
 
       try {
-        const response = await fetch(URL);
+        const response = await fetch(URL)
         if (!response.ok) {
           throw new Error(
             `Failed to fetch the requested planet, reload page and try again`
-          );
+          )
         }
-        const data = await response.json();
-        setOnePlanet(data);
+        const data = await response.json()
+        setOnePlanet(data)
       } catch (error) {
-        setError(error.message);
+        setError(error.message)
       } finally {
-        setLoading(false);
+        setLoading(false)
       }
-    };
-    fetchOnePlanet();
-  }, [planet]);
+    }
+    fetchOnePlanet()
+  }, [planet])
 
   if (loading) {
-    return <p>Loading planet...</p>;
+    return <p>Loading planet...</p>
   }
 
   if (error) {
-    return <p>{error}</p>;
+    return <p>{error}</p>
   }
 
   return (
     <div>
-      <div>
-        <Link to="/">
-          <p>Back to spaceport</p>
-        </Link>
-      </div>
       <PlanetDetails onePlanet={onePlanet} />
       <PlanetList />
     </div>
-  );
-};
+  )
+}
