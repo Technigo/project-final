4a9f17164f5a56d402734ab9f88b60824dd2d564
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-06-05 11:20:05 +0200

    Refactored return and replaces a href to use nav link.

--- frontend/src/pages/Home/components/ProductShowcase/ProductShowcase.jsx
+++ frontend/src/pages/Home/components/ProductShowcase/ProductShowcase.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -9,19 +10,19 @@ export const ProductShowcase = () => {
     dots: true,
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     swipe: true,
     swipeToSlide: true,
     responsive: [
       {
-        breakpoint: 1025, // Adjust this value based on your requirements
+        breakpoint: 1025,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
+          slidesToScroll: 2,
         },
       },
       {
-        breakpoint: 668, // Adjust this value based on your requirements
+        breakpoint: 668,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
@@ -32,46 +33,40 @@ export const ProductShowcase = () => {
 
   const slides = [
     {
+      id: "665879519c3e3eec9663ffb1",
       productName: "Emerald green sunbeam sunglasses",
       imageUrl:
         "https://res.cloudinary.com/dftguo4fa/image/upload/v1716982749/openart-image_rTwiAfPv_1716980280287_raw_gmgyrf.jpg",
-      linkUrl:
-        "https://cones-and-stones.netlify.app/products/665879519c3e3eec9663ffb1",
     },
     {
+      id: "665849779c3e3eec961e12fe",
       productName: "Pink paradise tee",
       imageUrl:
         "https://res.cloudinary.com/dftguo4fa/image/upload/v1716972650/8-zuFquagtzwIqLbh_yez88h.png",
-      linkUrl:
-        "https://cones-and-stones.netlify.app/products/665849779c3e3eec961e12fe",
     },
     {
+      id: "665876b89c3e3eec965a4b2a",
       productName: "Sunshine yellow bliss dress",
       imageUrl:
         "https://res.cloudinary.com/dftguo4fa/image/upload/v1716975501/openart-image_lNHSzF8B_1716975209055_raw_elp0ms.jpg",
-      linkUrl:
-        "https://cones-and-stones.netlify.app/products/665876b89c3e3eec965a4b2a",
     },
     {
+      id: "66586efd9c3e3eec962a7125",
       productName: "Black denim dream jeans",
       imageUrl:
         "https://res.cloudinary.com/dftguo4fa/image/upload/v1716975475/openart-image_CD-eAqhN_1716974003775_raw_glo0cv.jpg",
-      linkUrl:
-        "https://cones-and-stones.netlify.app/products/66586efd9c3e3eec962a7125",
     },
     {
+      id: "665877c59c3e3eec966140a6",
       productName: "Sky blue sunburst sunglasses",
       imageUrl:
         "https://res.cloudinary.com/dftguo4fa/image/upload/v1716982742/openart-image_NjTWTCDB_1716980211767_raw_kpgvmp.jpg",
-      linkUrl:
-        "https://cones-and-stones.netlify.app/products/665877c59c3e3eec966140a6",
     },
     {
+      id: "66586ff69c3e3eec96317b12",
       productName: "Blue and pink color splash cap",
       imageUrl:
         "https://res.cloudinary.com/dftguo4fa/image/upload/c_fill,ar_1:1,g_auto/v1716975476/openart-image_CpTkYdts_1716974741673_raw_ybgqhu.jpg",
-      linkUrl:
-        "https://cones-and-stones.netlify.app/products/66586ff69c3e3eec96317b12",
     },
   ];
 
@@ -80,13 +75,13 @@ export const ProductShowcase = () => {
       <Slider {...settings}>
         {slides.map((slide, index) => (
           <div key={index}>
-            <a href={slide.linkUrl} rel="noopener noreferrer">
+            <Link to={`/products/${slide.id}`}>
               <img
                 src={slide.imageUrl}
                 alt={slide.productName}
                 style={{ width: "100%", height: "auto" }}
               />
-            </a>
+            </Link>
           </div>
         ))}
       </Slider>
