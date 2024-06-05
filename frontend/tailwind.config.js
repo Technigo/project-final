ccf9291ef837a0cc46153cac6f9b842ff5dee6e6
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-06-05 11:37:43 +0200

    Add 2 colors to the color theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,21 +3,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      lato: ['Lato', 'sans-serif'],
-      poppins: ['Poppins', 'sans-serif'],
-      montserrat: ['Montserrat', 'sans-serif']
+      lato: ["Lato", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"],
     },
     colors: {
-      'blue': '#3D52A0',
-      'light-blue': '#ADBBDA',
-      'light-gray': '#EDE8F5',
-      'red': '#F78888',
-      'button-blue': '#4B5DFF',
-      'black': '#000000',
-      'white': '#FFFFFF',
+      blue: "#3D52A0",
+      "light-blue": "#ADBBDA",
+      "light-gray": "#EDE8F5",
+      red: "#F78888",
+      "button-blue": "#4B5DFF",
+      "button-hover": "#031CFF",
+      "button-disabled": "#475989",
+      black: "#000000",
+      white: "#FFFFFF",
     },
-    extend: {
-    },
+    extend: {},
   },
   plugins: [],
 };
