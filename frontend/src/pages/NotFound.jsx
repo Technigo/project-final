d685bf5dff6d05864788e9f2ee5d9e8e163db1e5
Mai Kanetaka <maikanetaka@gmail.com>
2024-06-06 12:12:43 +0200

    Create not found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,3 +1,21 @@
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+
 export const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <Header />
+      <div className="mx-6 my-20">
+        <h2>404</h2>
+        <h3>Oops! That page can’t be found</h3>
+        <p>
+          Sorry, but the page you are looking for doesn't exist, has been
+          removed, or is temporarily unavailable. But don’t worry, there’s
+          plenty more to explore: Go to our Homepage to start fresh. Use the
+          search bar to find what you need. Thank you for your understanding!
+        </p>
+      </div>
+      <Footer />
+    </>
+  );
 };
