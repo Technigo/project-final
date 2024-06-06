e4402c455ac96eb488066793eac723d0b32c764c
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-06-06 16:02:03 +0200

    Added correct headings and links.

--- frontend/src/common/Header/HamburgerMenu.jsx
+++ frontend/src/common/Header/HamburgerMenu.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
 import { Image } from "../ReusableComponents/Image/Image";
 import "./HamburgerMenu.css";
 import HamburgerIcon from "./hamburger.svg";
 
 export const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <Menu
       customBurgerIcon={
@@ -14,22 +21,46 @@ export const HamburgerMenu = () => {
           className="hamburger-icon"
         />
       }
-      // menuClassName={"bm-menu"}
-      // pageWrapId={"page-wrap"}
-      // outerContainerId={"outer-container"}
+      isOpen={isOpen}
+      onStateChange={(state) => setIsOpen(state.isOpen)}
     >
-      <a className="menu-item" href="/">
-        Home
-      </a>
-      <a className="menu-item" href="/about">
-        About
-      </a>
-      <a className="menu-item" href="/contact">
-        Contact
-      </a>
-      <a className="menu-item" href="/services">
-        Services
-      </a>
+      <ul className="menu-categories">
+        <li>
+          <h5>
+            <NavLink to="/products" end onClick={closeMenu}>
+              All products
+            </NavLink>
+          </h5>
+        </li>
+        <li>
+          <h5>
+            <NavLink to="/products/category/tops" onClick={closeMenu}>
+              Tops
+            </NavLink>
+          </h5>
+        </li>
+        <li>
+          <h5>
+            <NavLink to="/products/category/bottoms" onClick={closeMenu}>
+              Bottoms
+            </NavLink>
+          </h5>
+        </li>
+        <li>
+          <h5>
+            <NavLink to="/products/category/dresses" onClick={closeMenu}>
+              Dresses
+            </NavLink>
+          </h5>
+        </li>
+        <li>
+          <h5>
+            <NavLink to="/products/category/accessories" onClick={closeMenu}>
+              Accessories
+            </NavLink>
+          </h5>
+        </li>
+      </ul>
     </Menu>
   );
 };
