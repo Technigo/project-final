06f42794a11f40a692dedca0f511bc8e05b1535f
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-06-06 21:32:09 +0200

    set note title to text

--- frontend/src/Note.jsx
+++ frontend/src/Note.jsx
@@ -14,7 +14,7 @@ export const Note = ({ text, latitude, longitude, open, onClick }) => {
         ref={markerRef}
         onClick={onClick}
         position={{ lat: latitude, lng: longitude }}
-        title={"AdvancedMarker that opens an Infowindow when clicked."}
+        title={text}
       />
       {open && (
         <InfoWindow anchor={marker} maxWidth={200} onCloseClick={onClick}>
