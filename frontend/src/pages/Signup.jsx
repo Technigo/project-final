fc60d9f7892b67a1ade3a36ee349d2cc2932d382
Mai Kanetaka <maikanetaka@gmail.com>
2024-06-06 17:37:26 +0200

    Remove header and footer from Signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,15 +1,5 @@
-import { Header } from "../components/Header";
 import { Form } from "../components/Form";
-import { Footer } from "../components/Footer";
 
 export const Signup = () => {
-  return (
-    <>
-      <Header />
-      <div className="mb-20 pt-10 lg:mb-32 lg:mt-20">
-        <Form isLogin={false} />
-      </div>
-      <Footer />
-    </>
-  );
+  return <Form isLogin={false} />;
 };
