b59d1bb28d6a71e3498dc1aed876fad29bbaa148
Mai Kanetaka <maikanetaka@gmail.com>
2024-06-06 18:32:23 +0200

    re-create signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,14 @@
 import { Form } from "../components/Form";
 
 export const Signup = () => {
-  return <Form isLogin={false} />;
+  return (
+    <div className="flex w-full justify-center lg:justify-start">
+      <div className="mx-auto flex w-full flex-col items-center lg:max-w-screen-md lg:flex-initial lg:items-start">
+        <h1 className="mt-10 font-poppins font-bold lg:mb-10">Sign up</h1>
+        <div className="mx-auto">
+          <Form isLogin={false} />
+        </div>
+      </div>
+    </div>
+  );
 };
