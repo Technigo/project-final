1159ec745f5cc7c3797f0bcc3bfbcf2ca40148b0
izzibizz <contact@izabellind.com>
2024-06-06 16:23:28 +0200

    added some styling to text

--- frontend/src/pages/SingleProductPage.jsx
+++ frontend/src/pages/SingleProductPage.jsx
@@ -2,10 +2,13 @@ import { useProductsStore } from "../store/useProductsStore"
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { Loading } from "../components/Loading";
+import { MdOutlineStar } from "react-icons/md";
 
 export const SingleProductPage = () => {
   const { id } = useParams();
   const { fetchSingleProduct, loadingProduct, singleProduct } = useProductsStore();
+
+  const addToCart = "Add to Cart"; //productLangData.add-to-cart
 
   useEffect(() => {
     fetchSingleProduct(id)
@@ -21,11 +24,19 @@ export const SingleProductPage = () => {
     <div className="w-full tablet:w-11/12 tablet:mt-4 tablet:m-auto tablet:flex">
       <img src={singleProduct.image.url} alt={singleProduct.description} className="w-full tablet:w-7/12 desktop:w-4/12 object-cover aspect-square tablet:rounded-xl "/>
       <div className="w-9/12 m-auto tablet:m-0 py-6 text-text-light font-heading tablet:pl-6">
-        <h2 className="text-xl">{singleProduct.brand}</h2>
-        <h3  className="text-3xl">{singleProduct.title}</h3>
-        <p>{singleProduct.description}</p>
-        <p>starsection</p>
-        <h3 className="text-3xl">{singleProduct.price} €</h3>
+        <h2 className="font-light text-lg tablet:text-xl mb-2">{singleProduct.brand}</h2>
+        <h3  className="text-xl tablet:text-3xl mb-8">{singleProduct.title}</h3>
+        <p className="mb-8 leading-loose">{singleProduct.description}</p>
+        <div className="mb-12 flex">
+        <MdOutlineStar /><MdOutlineStar /><MdOutlineStar /><MdOutlineStar /><MdOutlineStar />
+        </div>
+        <h3 className="text-3xl mb-8">{singleProduct.price} €</h3>
+        <div className="flex">
+        <button classname="bg-main-yellow rounded-l-xl w">-</button><button classname="bg-text-light rounded-r-xl w">+</button>
+        </div>
+        <button className="w-24 text-xs bg-button-light p-1 rounded-full text-text-dark hover:bg-main-yellow">
+          {addToCart}
+        </button>
       </div>
     </div>
   )}
