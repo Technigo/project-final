1e5577cec775449ed613b54b87db3acc9a7bd021
Martin Jönsson <martin.t.joensson@gmail.com>
2024-06-06 17:53:50 +0200

    Added not found component

--- frontend/src/components/ReviewForm.jsx
+++ frontend/src/components/ReviewForm.jsx
@@ -3,3 +3,48 @@
 export const ReviewForm = () => {
   return <div>ReviewForm</div>;
 };
+
+
+
+import { useProductsStore } from "../store/useProductsStore";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Loading } from "../components/Loading";
+
+export const SingleProductPage = () => {
+  const { id } = useParams();
+  const { fetchSingleProduct, loadingProduct, singleProduct } =
+    useProductsStore();
+  const product = singleProduct.product;
+
+  useEffect(() => {
+    fetchSingleProduct(id);
+  }, []);
+
+  if (!product || !product.image || !product.image.url) {
+    return <div>No product to review.</div>;
+  }
+
+  return (
+    <section className="bg-main-red h-full min-h-screen w-full pt-12 laptop:pt-28">
+      {loadingProduct ? (
+        <Loading />
+      ) : (
+        <div className="w-full">
+          <img
+            src={product.image.url}
+            alt={product.description}
+            className="w-full object-cover object-center aspect-square"
+          />
+          <div className="w-9/12 m-auto py-6 text-text-light font-heading">
+            <h2>{product.brand}</h2>
+            <h3>{product.title}</h3>
+            <p>{product.description}</p>
+            <p>starsection</p>
+  
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,10 @@
+import { Loading } from "../components/Loading";
+
 export const NotFound = () => {
   return (
-    <div>
-      <h2>NotFound</h2>
+    <div className="bg-main-red ">
+      <h2 className="text-center font-heading py-40 text-text-light text-2xl">The page you are looking for was not found.</h2>
+      <Loading />
     </div>
   );
 };
