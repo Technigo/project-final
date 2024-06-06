9f207855668854736599cb7c9df587f01ad88e6c
Mai Kanetaka <maikanetaka@gmail.com>
2024-06-06 11:47:36 +0200

    Create signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,15 @@
+import { Header } from "../components/Header";
 import { Form } from "../components/Form";
+import { Footer } from "../components/Footer";
 
 export const Signup = () => {
-  return <Form isLogin={false} />;
+  return (
+    <>
+      <Header />
+      <div className="mb-20 pt-10 lg:mb-32 lg:mt-20">
+        <Form isLogin={false} />
+      </div>
+      <Footer />
+    </>
+  );
 };
