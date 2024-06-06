55ec881f37e1b932bd228970f9e07a4691f0a45f
Mai Kanetaka <maikanetaka@gmail.com>
2024-06-06 17:12:01 +0200

    Update NotFound page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,25 +1,27 @@
-import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
 import { Link } from "react-router-dom";
 
 export const NotFound = () => {
   return (
     <>
-      <div className="mx-6 my-20">
-        <h2>404</h2>
-        <h3>Oops! That page can’t be found</h3>
-        <p>
+      <div className="mx-6">
+        <h1 className="mt-10 font-poppins font-bold text-light-blue">404</h1>
+        <h2 className="my-3 font-poppins font-bold text-blue">
+          Oops! That page can’t be found
+        </h2>
+        <p className="font-lato text-sm">
           Sorry, but the page you are looking for doesn't exist, has been
           removed, or is temporarily unavailable. <br />
           But don’t worry, there’s plenty more to explore:
         </p>
-        <ul>
-          <li>
-            Go to our <Link to="/">Homepage</Link> to start fresh
+        <ul className="m-4">
+          <li className="list-disc font-lato text-sm">
+            Go to our <Link to="/" className="underline font-bold">Homepage</Link> to start fresh
           </li>
-          <li>Use the search bar to find what you need</li>
+          <li className="list-disc font-lato text-sm">
+            Use the search bar to find what you need
+          </li>
         </ul>
-        <p>Thank you for your understanding!</p>
+        <p className="font-lato text-sm">Thank you for your understanding!</p>
       </div>
     </>
   );
