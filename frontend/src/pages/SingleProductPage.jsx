53eb0320f704a8b34a69447e133cdb2311d47ca8
izzibizz <contact@izabellind.com>
2024-06-06 14:18:04 +0200

    changed how we manage the data and added an error message

--- frontend/src/pages/SingleProductPage.jsx
+++ frontend/src/pages/SingleProductPage.jsx
@@ -1,33 +1,42 @@
-import { useProductsStore } from "../store/useProductsStore"
-import { useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { useProductsStore } from "../store/useProductsStore";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { Loading } from "../components/Loading";
 
 export const SingleProductPage = () => {
   const { id } = useParams();
-  const { fetchSingleProduct, loadingProduct, singleProduct } = useProductsStore();
+  const { fetchSingleProduct, loadingProduct, singleProduct } =
+    useProductsStore();
+  const product = singleProduct.product;
 
   useEffect(() => {
-    fetchSingleProduct(id)
-  }, [])
+    fetchSingleProduct(id);
+  }, []);
 
+  if (!product || !product.image || !product.image.url) {
+    return <div>No product found or image unavailable.</div>;
+  }
 
   return (
     <section className="bg-main-red h-full min-h-screen w-full pt-12 laptop:pt-28">
-  {loadingProduct ? (
-    <Loading />
-  ) : (
-    <div className="w-full">
-      <img src={singleProduct.image.url} alt={singleProduct.description} className="w-full object-cover object-center aspect-square"/>
-      <div className="w-9/12 m-auto py-6 text-text-light font-heading">
-        <h2>{singleProduct.brand}</h2>
-        <h3>{singleProduct.title}</h3>
-        <p>{singleProduct.description}</p>
-        <p>starsection</p>
-        <h3>{singleProduct.price} €</h3>
-      </div>
-    </div>
-  )}
+      {loadingProduct ? (
+        <Loading />
+      ) : (
+        <div className="w-full">
+          <img
+            src={product.image.url}
+            alt={product.description}
+            className="w-full object-cover object-center aspect-square"
+          />
+          <div className="w-9/12 m-auto py-6 text-text-light font-heading">
+            <h2>{product.brand}</h2>
+            <h3>{product.title}</h3>
+            <p>{product.description}</p>
+            <p>starsection</p>
+            <h3>{product.price} €</h3>
+          </div>
+        </div>
+      )}
     </section>
-  )
+  );
 };
