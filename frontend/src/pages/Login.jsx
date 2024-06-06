1eaac5998567efa6818d884c70425853c1763da0
Mai Kanetaka <maikanetaka@gmail.com>
2024-06-06 17:18:25 +0200

    remove header and footer component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,9 @@
-import { Header } from "../components/Header";
 import { Form } from "../components/Form";
-import { Footer } from "../components/Footer";
 
 export const Login = () => {
   return (
     <>
-      <Header />
       <Form isLogin={true} />
-      <Footer />
     </>
   );
 };
