e9351a107a24e7a96951d36e9503e8b95184d989
Mai Kanetaka <maikanetaka@gmail.com>
2024-06-06 18:28:11 +0200

    Add h1

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,8 +2,13 @@ import { Form } from "../components/Form";
 
 export const Login = () => {
   return (
-    <>
-      <Form isLogin={true} />
-    </>
+    <div className="flex w-full justify-center lg:justify-start">
+      <div className="mx-auto flex w-full flex-col items-center lg:max-w-screen-md lg:flex-initial lg:items-start">
+        <h1 className="font-poppins font-bold mt-10 lg:mb-10">Login</h1>
+        <div className="mx-auto">
+          <Form isLogin={true} />
+        </div>
+      </div>
+    </div>
   );
 };
