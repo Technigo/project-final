9ac33cd24a9b512df5b115c31658a9bc70eec984
unknown <linda.frischknecht@bluewin.ch>
2024-06-07 10:24:58 +0200

    doing pages 1&2 of tablet

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -32,29 +32,31 @@ export const SignUpPage = () => {
                 id="email"
               ></input>
             </div>
-            <div className="flex flex-col w-full tablet:w-2/5">
-              <label
-                htmlFor="password"
-                className="text-text-dark font-heading font-semibold mt-8  "
-              >
-                Password:
-              </label>
-              <input
-                className="rounded-xl h-10 bg-bg-input "
-                id="password"
-              ></input>
-            </div>
-            <div className="flex flex-col w-full tablet:w-2/5 tablet:self-end">
-              <label
-                htmlFor="confirm"
-                className="text-text-dark font-heading font-semibold mt-8"
-              >
-                Confirm password:
-              </label>
-              <input
-                className="rounded-xl h-10 bg-bg-input "
-                id="confirm"
-              ></input>
+            <div className="flex flex-col tablet:flex-row tablet:gap-5">
+              <div className="flex flex-col w-full">
+                <label
+                  htmlFor="password"
+                  className="text-text-dark font-heading font-semibold mt-8  "
+                >
+                  Password:
+                </label>
+                <input
+                  className="rounded-xl h-10 bg-bg-input "
+                  id="password"
+                ></input>
+              </div>
+              <div className="flex flex-col w-full">
+                <label
+                  htmlFor="confirm"
+                  className="text-text-dark font-heading font-semibold mt-8"
+                >
+                  Confirm password:
+                </label>
+                <input
+                  className="rounded-xl h-10 bg-bg-input "
+                  id="confirm"
+                ></input>
+              </div>
             </div>
             <button
               onClick={() => setActiveSection("sectiontwo")}
@@ -66,72 +68,105 @@ export const SignUpPage = () => {
         </section>
       )}
       {activeSection === "sectiontwo" && (
-        <section className="bg-main-red h-full flex flex-col items-center text-text-light px-10 pb-20">
+        <section className="bg-main-red h-full flex flex-col items-center text-text-light px-10 pb-20 tablet:p-0">
           {" "}
           <h2 className="text-2xl font-heading font-bold mt-10">Sign Up</h2>
           <h3 className="text-xl font-heading font-medium mt-3">
             personal Information
           </h3>
-          <form className="flex flex-col w-full">
-            <label
-              htmlFor="firstname"
-              className="text-text-dark font-heading font-semibold mt-8"
-            >
-              Firstname:
-            </label>
-            <input
-              className="rounded-xl h-10 bg-bg-input"
-              id="firstname"
-            ></input>
-            <label
-              htmlFor="lastname"
-              className="text-text-dark font-heading font-semibold mt-2"
-            >
-              Lastname:
-            </label>
-            <input
-              id="lastname"
-              className="rounded-xl h-10 bg-bg-input"
-            ></input>
-            <label
-              htmlFor="address"
-              className="text-text-dark font-heading font-semibold mt-10"
-            >
-              Address:
-            </label>
-            <input id="address" className="rounded-xl h-10 bg-bg-input"></input>
-
-            <label
-              htmlFor="postal code"
-              className="text-text-dark font-heading font-semibold mt-2"
-            >
-              Postal code:
-            </label>
-            <input
-              id="postal code"
-              className="rounded-xl h-10 bg-bg-input"
-            ></input>
-            <label
-              htmlFor="city"
-              className="text-text-dark font-heading font-semibold mt-2"
-            >
-              City:
-            </label>
-            <input id="city" className="rounded-xl h-10 bg-bg-input"></input>
-            <label
-              htmlFor="country"
-              className="text-text-dark font-heading font-semibold mt-2"
-            >
-              Country:
-            </label>
-            <input id="country" className="rounded-xl h-10 bg-bg-input"></input>
+          <form className="flex flex-col w-full tablet:px-36 tablet:pb-10">
+            <div className="flex flex-col mt-8 tablet:flex-row tablet:gap-5">
+              <div className="flex flex-col w-full">
+                <label
+                  htmlFor="firstname"
+                  className="text-text-dark font-heading font-semibold"
+                >
+                  Firstname:
+                </label>
+                <input
+                  className="rounded-xl h-10 bg-bg-input"
+                  id="firstname"
+                ></input>
+              </div>
+              <div className="flex flex-col w-full mt-2 tablet:mt-0">
+                <label
+                  htmlFor="lastname"
+                  className="text-text-dark font-heading font-semibold"
+                >
+                  Lastname:
+                </label>
+                <input
+                  id="lastname"
+                  className="rounded-xl h-10 bg-bg-input"
+                ></input>
+              </div>
+            </div>
+            <div className="mt-10">
+              <div className="flex flex-col tablet:flex-row tablet:gap-5">
+                <div className="flex flex-col w-full">
+                  <label
+                    htmlFor="address"
+                    className="text-text-dark font-heading font-semibold"
+                  >
+                    Address:
+                  </label>
+                  <input
+                    id="address"
+                    className="rounded-xl h-10 bg-bg-input"
+                  ></input>
+                </div>
+                <div className="flex flex-col w-full">
+                  <label
+                    htmlFor="postal code"
+                    className="text-text-dark font-heading font-semibold mt-2 tablet:mt-0"
+                  >
+                    Postal code:
+                  </label>
+                  <input
+                    id="postal code"
+                    className="rounded-xl h-10 bg-bg-input"
+                  ></input>
+                </div>
+              </div>
+              <div className="flex flex-col tablet:flex-row tablet:gap-5">
+                <div className="flex flex-col w-full">
+                  <label
+                    htmlFor="city"
+                    className="text-text-dark font-heading font-semibold mt-2"
+                  >
+                    City:
+                  </label>
+                  <input
+                    id="city"
+                    className="rounded-xl h-10 bg-bg-input"
+                  ></input>
+                </div>
+                <div className="flex flex-col w-full">
+                  <label
+                    htmlFor="country"
+                    className="text-text-dark font-heading font-semibold mt-2"
+                  >
+                    Country:
+                  </label>
+                  <input
+                    id="country"
+                    className="rounded-xl h-10 bg-bg-input"
+                  ></input>
+                </div>
+              </div>
+            </div>
             <button
               onClick={() => setActiveSection("sectionthree")}
-              className="bg-main-yellow h-8 w-28 self-center mt-12 rounded-3xl text-text-dark font-heading"
+              className="bg-main-yellow h-8 w-28 self-center mt-12 rounded-3xl text-text-dark font-heading tablet:mb-10 tablet:self-end"
             >
               Next
             </button>
           </form>
+          <img
+            className="hidden tablet:block tablet:w-screen"
+            src="signup.svg"
+            alt="picture of a woman using skincare."
+          ></img>
         </section>
       )}
       {activeSection === "sectionthree" && (
