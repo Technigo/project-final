c123ed8ab7ba72227959e73168e53ed6b257700d
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-06-07 10:06:08 +0200

    Fixed issue with slide menu opening when clicking anywhere on the site.

--- frontend/src/common/Header/HamburgerMenu.jsx
+++ frontend/src/common/Header/HamburgerMenu.jsx
@@ -6,61 +6,74 @@ import "./HamburgerMenu.css";
 import HamburgerIcon from "./hamburger.svg";
 
 export const HamburgerMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
+
+  // const handleStateChange = (state) => {
+  //   setIsOpen(state.isOpen);
+  // };
 
   const closeMenu = () => {
     setIsOpen(false);
   };
 
+  // const toggleMenu = () => {
+  //   setIsOpen(!isOpen);
+  // };
+
   return (
-    <Menu
-      customBurgerIcon={
-        <Image
-          src={HamburgerIcon}
-          alt="Hamburger icon"
-          className="hamburger-icon"
-        />
-      }
-      isOpen={isOpen}
-      onStateChange={(state) => setIsOpen(state.isOpen)}
-    >
-      <ul className="menu-categories">
-        <li>
-          <h5>
-            <NavLink to="/products" end onClick={closeMenu}>
-              All products
-            </NavLink>
-          </h5>
-        </li>
-        <li>
-          <h5>
-            <NavLink to="/products/category/tops" onClick={closeMenu}>
-              Tops
-            </NavLink>
-          </h5>
-        </li>
-        <li>
-          <h5>
-            <NavLink to="/products/category/bottoms" onClick={closeMenu}>
-              Bottoms
-            </NavLink>
-          </h5>
-        </li>
-        <li>
-          <h5>
-            <NavLink to="/products/category/dresses" onClick={closeMenu}>
-              Dresses
-            </NavLink>
-          </h5>
-        </li>
-        <li>
-          <h5>
-            <NavLink to="/products/category/accessories" onClick={closeMenu}>
-              Accessories
-            </NavLink>
-          </h5>
-        </li>
-      </ul>
-    </Menu>
+    <div id="outer-container">
+      <Menu
+        customBurgerIcon={
+          <Image
+            src={HamburgerIcon}
+            alt="Hamburger icon"
+            className="hamburger-icon"
+            // onClick={toggleMenu}
+          />
+        }
+        // isOpen={isOpen}
+        // onStateChange={handleStateChange}
+        outerContainerId={"outer-container"}
+        pageWrapId={"page-wrap"}
+      >
+        <ul className="menu-categories">
+          <li>
+            <h5>
+              <NavLink to="/products" end onClick={closeMenu}>
+                All products
+              </NavLink>
+            </h5>
+          </li>
+          <li>
+            <h5>
+              <NavLink to="/products/category/tops" onClick={closeMenu}>
+                Tops
+              </NavLink>
+            </h5>
+          </li>
+          <li>
+            <h5>
+              <NavLink to="/products/category/bottoms" onClick={closeMenu}>
+                Bottoms
+              </NavLink>
+            </h5>
+          </li>
+          <li>
+            <h5>
+              <NavLink to="/products/category/dresses" onClick={closeMenu}>
+                Dresses
+              </NavLink>
+            </h5>
+          </li>
+          <li>
+            <h5>
+              <NavLink to="/products/category/accessories" onClick={closeMenu}>
+                Accessories
+              </NavLink>
+            </h5>
+          </li>
+        </ul>
+      </Menu>
+    </div>
   );
 };
