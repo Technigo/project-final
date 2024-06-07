a36d34d8a512931039de164f3974b55f6d7d6e03
Lovisa <lovisa.aaberg@gmail.com>
2024-06-08 01:55:43 +0200

    Added styling to feed

--- frontend/src/components/SpaceFeed.jsx
+++ frontend/src/components/SpaceFeed.jsx
@@ -1,40 +1,145 @@
 import { useEffect, useState, useRef, useCallback } from "react"
 import { useSpaceFeedStore } from "../stores/useSpaceFeedStore"
 import styled from "styled-components"
-import { Button } from "./Button"
 import rocketIcon from "./../assets/icons/rocket.png"
 import rocketIconLiked from "./../assets/icons/rocket-filled.png"
+import { formatDistanceToNow } from "date-fns"
+
+const FeedContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: var(--text-color-secondary);
+  font-family: var(--font-family-text);
+  font-size: var(--font-size-text-mob);
+
+  h2 {
+    font-family: var(--font-family-headlines);
+    font-size: var(--font-size-h2-mob);
+    color: var(--headline-color);
+
+    @media (min-width: 768px) {
+      font-size: var(--font-size-h2-desktop);
+    }
+  }
+
+  @media (min-width: 768px) {
+    width: 100%;
+    max-width: 925px;
+    max-height: 1100px;
+    font-size: var(--font-size-large);
+  }
+
+  textarea {
+    width: 280px;
+    height: 60px;
+    border-radius: 8px;
+
+    @media (min-width: 768px) {
+      width: 660px;
+      height: 140px;
+    }
+  }
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  width: 100%;
+  max-width: 660px;
+`
+
+const Button = styled.button`
+  width: 150px;
+  height: 45px;
+  background-color: #cf4b14;
+  color: #ffffff;
+  border-radius: 40px;
+  border: none;
+  font-family: var(--font-family-text);
+  font-size: var(--font-size-text-mob);
+  font-weight: 700;
+  display: block;
+  justify-content: center;
+
+  &:hover {
+    background-color: #ee723f;
+  }
+
+  &.active {
+    background-color: #ee723f;
+  }
+
+  @media (min-width: 768px) {
+    width: 200px;
+    height: 60px;
+    font-size: var(--font-size-large);
+    display: block;
+  }
+`
 
 const LikeButton = styled.button`
-  background: #000000;
+  background: #161822;
   color: #ffffff;
-  font-family: var(--font-size-text-mob);
+  font-family: var(--font-family-text);
   border: none;
+  cursor: pointer;
 `
 const Icon = styled.img`
   width: 30px;
   height: 30px;
 `
 
-const FeedContainer = styled.div`
+const MessageContainer = styled.div`
   width: 100%;
-  max-width: 280px;
-  max-height: 850px;
+  max-height: 1000px;
   overflow-y: auto;
+  text-align: center;
+  font-size: var(--font-size-text-mob);
+  font-family: var(--font-family-text);
+  color: var(--text-color-primary);
+  margin-bottom: 20px;
 
   @media (min-width: 768px) {
-    width: 760px;
+    width: 660px;
     max-height: 1100px;
+    font-size: var(--font-size-large);
   }
 
   ul {
     list-style: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px 16px;
   }
 
   li {
     text-decoration: none;
+    background-color: #161822;
+    border-radius: 8px;
+    padding: 20px 16px;
+    margin-bottom: 20px;
+    width: 100%;
+
+    @media (min-width: 768px) {
+      text-align: left;
+    }
   }
 `
+
+const Details = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: #161822;
+`
+
 export const SpaceFeed = () => {
   const {
     spaceFeed,
@@ -105,15 +210,21 @@ export const SpaceFeed = () => {
   }
 
   return (
-    <div>
-      <h3>SpaceFeed</h3>
+    <FeedContainer>
+      <h2>CELESTIAL FEED</h2>
+      <p>
+        Share your cosmic thoughts, discoveries, and adventures with fellow
+        stargazers! Post updates, ask questions about the cosmos, and engage in
+        enlightening conversations. Let's explore the wonders of the universe
+        together!
+      </p>
 
-      <form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit}>
         <label>
           <textarea
             type="text"
             value={newMessage}
-            placeholder=""
+            placeholder="Write here..."
             required
             onChange={handleNewMessage}
           />
@@ -122,35 +233,42 @@ export const SpaceFeed = () => {
           POST
         </Button>
         {messageError && <p>{messageError}</p>}
-      </form>
+      </Form>
 
       {messageError && <p>{messageError}</p>}
 
       {loading && <p>Loading...</p>}
 
-      <FeedContainer>
+      <MessageContainer>
         <ul>
           {spaceFeed && spaceFeed.length > 0 ? (
             spaceFeed.map((message) => (
               <li key={message._id}>
                 <p>{message.message}</p>
-                <LikeButton onClick={() => handleLike(message._id)}>
-                  <Icon
-                    src={
-                      likedMessages[message._id] ? rocketIconLiked : rocketIcon
-                    }
-                    alt="Like Icon"
-                  />{" "}
-                  x{message.likes}
-                </LikeButton>
+                <Details>
+                  <LikeButton onClick={() => handleLike(message._id)}>
+                    <Icon
+                      src={
+                        likedMessages[message._id]
+                          ? rocketIconLiked
+                          : rocketIcon
+                      }
+                      alt="Like Icon"
+                    />{" "}
+                    x{message.likes}
+                  </LikeButton>
+                  {formatDistanceToNow(new Date(message.createdAt), {
+                    addSuffix: true,
+                  })}
+                </Details>
               </li>
             ))
           ) : (
             <p>No messages to display</p>
           )}
         </ul>
-      </FeedContainer>
-    </div>
+      </MessageContainer>
+    </FeedContainer>
   )
 }
 
