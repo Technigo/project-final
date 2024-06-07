f30b6efc6d6ebf8b577b6e612fc8622a8cdfeed3
SofieFerrari <sofiestrahl@gmail.com>
2024-06-07 12:58:41 +0200

    worked in cart and cartitem

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,6 +2,7 @@
 import { Link } from "react-router-dom";
 import { Button } from "../components/Button";
 import { Breadcrumb } from "../components/Breadcrumb";
+import { CartItem } from "../components/CartItem";
 
 const cartItems = [
   { id: "", templateName: "", price: 150 },
@@ -22,24 +23,42 @@ export const Cart = () => {
   return (
     <>
       <Breadcrumb />
-      <div className="text-blue">
-        <div className="">
-          <span>Order Value:</span>
-          <span>€{orderValue.toFixed(2)}</span>
+      <div className="flex flex-col items-center">
+        <h3 className="mb-5 font-montserrat font-bold sm:self-center lg:ml-5 lg:self-start">
+          Your cart items
+        </h3>
+        <CartItem className="sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4" />
+        <CartItem className="sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4" />
+        <CartItem className="sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4" />
+        <div className="mt-5 flex h-[120px] min-w-[300px] flex-col text-sm font-bold leading-8 before:font-montserrat">
+          <div className="flex justify-between">
+            <span>Order Value</span>
+            <span>€{orderValue.toFixed(2)}</span>
+          </div>
+          <div className="flex justify-between">
+            <span>VAT</span>
+            <span>€{vat.toFixed(2)}</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Total</span>
+            <span>€{total.toFixed(2)}</span>
+          </div>
         </div>
-        <div className="">
-          <span>VAT:</span>
-          <span>€{vat.toFixed(2)}</span>
+        <div className="mb-10 flex flex-col items-center gap-5">
+          <Link to="/checkout">
+            <Button text={"CONTINUE"} style="submit" />
+          </Link>
+          <Link to="/products">
+            <Button text={"CONTINUE SHOPPING"} style="submit" />
+          </Link>
+            <Button text={"CLEAR CART"} style="submit" />
         </div>
-      </div>
-      <div className="">
-        <span>Total:</span>
-        <span>€{total.toFixed(2)}</span>
-      </div>
-      <div className="flex justify-center">
-        <Link to="/checkout">
-          <Button text={"CONTINUE"} style="submit" />
-        </Link>
+        <div className="flex justify-center">
+          <p className="mb-10 w-[300px] p-3 text-center font-lato">
+            *Please remember, this is not an actual shop, no money will be
+            drawn, and unfortunately no products will be sent*.
+          </p>
+        </div>
       </div>
     </>
   );
