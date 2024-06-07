630ddf343886366e38a3c4a0949596c0b6e7b8d4
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-06-07 09:26:57 +0200

    updated review.js

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose"
 const { Schema } = mongoose
 
 //To add "created by" which should display the name of the user
-//Also need some idenfier so you know for which museum it's written and where it should be displayed..
 
 // Define schema
 const reviewSchema = new Schema({
