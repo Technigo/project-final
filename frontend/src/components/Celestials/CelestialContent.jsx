6342aac742df1d7fe8a986d4c82b0e19655987e3
Lovisa <lovisa.aaberg@gmail.com>
2024-06-08 23:35:11 +0200

    Styling to breadcrumbs

--- frontend/src/components/Celestials/CelestialContent.jsx
+++ frontend/src/components/Celestials/CelestialContent.jsx
@@ -1,8 +1,6 @@
-
-import styled from "styled-components";
-import sunImg from "../../assets/images/sun.png";
-import moonImg from "../../assets/images/moon.png";
-
+import styled from "styled-components"
+import sunImg from "../../assets/images/sun.png"
+import moonImg from "../../assets/images/moon.png"
 
 const SunAndMoonPage = styled.div`
   background: #000000;
@@ -21,16 +19,14 @@ const CelestialContent = ({ oneBody }) => {
   return (
     <div>
       <SunAndMoonPage>
-        <h1 className="space-mono-bold">The {oneBody.name}</h1>
+        <h1>The {oneBody.name}</h1>
         <img src={imgSrc} alt={oneBody.name} />
-        <p className="roboto-mono">Nickname: {oneBody.nickname}</p>
-        <p className="roboto-mono">Material: {oneBody.material}</p>
-        <p className="roboto-mono">
-          Surface Temperature: {oneBody.surfaceTemperature}
-        </p>
-        <p className="roboto-mono">Weather Climate: {oneBody.weatherClimate}</p>
-        <p className="roboto-mono">Travel time: {oneBody.travelTime}</p>
-        <p className="roboto-mono">Curiosa: {oneBody.curiosa}</p>
+        <p>Nickname: {oneBody.nickname}</p>
+        <p>Material: {oneBody.material}</p>
+        <p>Surface Temperature: {oneBody.surfaceTemperature}</p>
+        <p>Weather Climate: {oneBody.weatherClimate}</p>
+        <p>Travel time: {oneBody.travelTime}</p>
+        <p>Curiosa: {oneBody.curiosa}</p>
       </SunAndMoonPage>
     </div>
   )
