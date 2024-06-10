8f0ca9a9e8e650995cbb8b02203efc6d61494e90
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-06-10 09:50:07 +0200

    change product detail url to products

--- frontend/src/routes/AppRoute.jsx
+++ frontend/src/routes/AppRoute.jsx
@@ -1,21 +1,22 @@
 import { Route, Routes } from "react-router-dom";
-import { Homepage } from "../pages/Homepage";
-import { ProductList } from "../pages/ProductList";
-import { ProductDetail } from "../pages/ProductDetail";
-import { Signup } from "../pages/Signup";
-import { Login } from "../pages/Login";
-import { MyPage } from "../pages/MyPage";
+
 import { Cart } from "../pages/Cart";
 import { Checkout } from "../pages/Checkout";
+import { Homepage } from "../pages/Homepage";
+import { Login } from "../pages/Login";
+import { MyPage } from "../pages/MyPage";
 import { NotFound } from "../pages/NotFound";
 import { OrderConfirmation } from "../pages/OrderConfirmation";
+import { ProductDetail } from "../pages/ProductDetail";
+import { ProductList } from "../pages/ProductList";
+import { Signup } from "../pages/Signup";
 
 export const AppRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/products" element={<ProductList />} />
-      <Route path="/product/:Id" element={<ProductDetail />} />
+      <Route path="/products/:Id" element={<ProductDetail />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/mypage" element={<MyPage />} />
