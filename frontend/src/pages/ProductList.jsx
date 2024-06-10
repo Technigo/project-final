00e03b21fa25de3a91bfc8195215e5f453d1f630
Mai Kanetaka <maikanetaka@gmail.com>
2024-06-10 16:48:10 +0200

    add ProductList without styling

--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -1,45 +1,58 @@
+import { useState, useEffect } from "react";
 import { ProductCard } from "../components/ProductCard";
+import { Breadcrumb } from "../components/Breadcrumb";
 
 export const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/products`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProducts(data); 
+        setLoading(false); 
+      })
+      .catch((error) => {
+        console.error("Error fetching product", error);
+        setError(error.toString()); 
+        setLoading(false); 
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; 
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>; 
+  }
+
+  if (!products.length) {
+    return <div>No products found.</div>; 
+  }
+
   return (
-    <div className="lg:mx-[minmax(20px, auto)] mx-10 grid grid-cols-1 justify-center justify-items-center gap-10 lg:grid-cols-[repeat(3,_minmax(0,_500px))] lg:gap-x-[5%]">
-      <ProductCard
-        templateImg="https://res.cloudinary.com/ddpsnaef5/image/upload/v1717358234/cld-sample-5.jpg"
-        tags="Health, Wellness, Holistic"
-        name="Health and Wellness"
-        price={34.99}
-        category="Health and Wellness"
-      />
-      <ProductCard
-        templateImg="https://res.cloudinary.com/ddpsnaef5/image/upload/v1717358234/cld-sample-5.jpg"
-        tags="Health, Wellness, Holistic"
-        name="Health and Wellness"
-        price={34.99}
-      />
-      <ProductCard
-        templateImg="https://res.cloudinary.com/ddpsnaef5/image/upload/v1717358234/cld-sample-5.jpg"
-        tags="Health, Wellness, Holistic"
-        name="Health and Wellness"
-        price={34.99}
-      />
-      <ProductCard
-        templateImg="https://res.cloudinary.com/ddpsnaef5/image/upload/v1717358234/cld-sample-5.jpg"
-        tags="Health, Wellness, Holistic"
-        name="Health and Wellness"
-        price={34.99}
-      />
-      <ProductCard
-        templateImg="https://res.cloudinary.com/ddpsnaef5/image/upload/v1717358234/cld-sample-5.jpg"
-        tags="Health, Wellness, Holistic"
-        name="Health and Wellness"
-        price={34.99}
-      />
-      <ProductCard
-        templateImg="https://res.cloudinary.com/ddpsnaef5/image/upload/v1717358234/cld-sample-5.jpg"
-        tags="Health, Wellness, Holistic"
-        name="Health and Wellness"
-        price={34.99}
-      />
-    </div>
+    <>
+      <Breadcrumb />
+      <div className="lg:mx-[minmax(20px, auto)] lg:grid-cols-[repeat(3, minmax(0, 500px))] mx-10 grid grid-cols-1 justify-center justify-items-center gap-10 lg:gap-x-[5%]">
+        {products.map((product) => (
+          <ProductCard
+            key={product._id}
+            templateImg={product.image}
+            tags={product.tags}
+            name={product.templateName}
+            price={product.price}
+            category={product.category}
+          />
+        ))}
+      </div>
+    </>
   );
 };
