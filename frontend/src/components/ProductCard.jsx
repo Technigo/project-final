421d4d9c4dfb41b7d1da6964277c8ef47f232cfb
Mai Kanetaka <maikanetaka@gmail.com>
2024-06-10 21:45:54 +0200

    Adjust styling  for desktop

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -5,26 +5,28 @@ import favorite from "../assets/favorite-icon.svg";
 
 export const ProductCard = ({ templateImg, tags, name, price, category }) => {
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col">
       <img src={templateImg} alt="image of the template" />
+      <span className="mb-1 mt-2 flex flex-row">
+        {tags.split(", ").map((tag) => (
+          <button key={tag} className="mr-2 text-sm text-blue lg:text-xs">
+            #{tag}
+          </button>
+        ))}
+      </span>
       <div className="flex flex-row justify-between font-montserrat">
         <div className="flex flex-col">
-          <span className="mb-1 flex flex-row">
-            {tags.split(", ").map((tag) => (
-              <button key={tag} className="mr-2 text-sm text-blue">
-                #{tag}
-              </button>
-            ))}
-          </span>
-          <p className="mb-2 text-sm font-bold text-blue">{category}</p>
-          <p className="font-montserrat text-xl font-bold">{name}</p>
-          <p className="text-sm">€{price}</p>
+          <p className="mb-2 text-sm font-bold text-blue lg:text-xs">
+            {category}
+          </p>
+          <p className="font-montserrat text-lg font-bold lg:text-sm">{name}</p>
+          <p className="mt-1 text-sm lg:text-xs">€{price}</p>
         </div>
-        <div className="mr-3 flex flex-row items-center gap-6">
-          <button className="h-6 w-6">
+        <div className="flex flex-row items-center gap-6 lg:gap-4">
+          <button className="h-6 w-6 lg:h-4 lg:w-4">
             <img src={favorite} alt="add to favorite button" />
           </button>
-          <button className="h-6 w-6">
+          <button className="h-6 w-6 lg:h-4 lg:w-4">
             <img src={cart} alt="add to cart button" />
           </button>
         </div>
