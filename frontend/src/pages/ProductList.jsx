86a8446c2d55966a7d6a4d3ad31d05e28b91f647
Mai Kanetaka <maikanetaka@gmail.com>
2024-06-10 21:46:14 +0200

    Style for ProductList page

--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -11,47 +11,49 @@ export const ProductList = () => {
     fetch(`${import.meta.env.VITE_API_URL}/products`)
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error("There is a netwerk error");
         }
         return response.json();
       })
       .then((data) => {
-        setProducts(data); 
-        setLoading(false); 
+        setProducts(data);
+        setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching product", error);
-        setError(error.toString()); 
-        setLoading(false); 
+        setError(error.toString());
+        setLoading(false);
       });
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>; 
+    return <div>Loading...</div>;
   }
 
   if (error) {
-    return <div>Error: {error}</div>; 
+    return <div>Error: {error}</div>;
   }
 
   if (!products.length) {
-    return <div>No products found.</div>; 
+    return <div>No products found.</div>;
   }
 
   return (
     <>
       <Breadcrumb />
-      <div className="lg:mx-[minmax(20px, auto)] lg:grid-cols-[repeat(3, minmax(0, 500px))] mx-10 grid grid-cols-1 justify-center justify-items-center gap-10 lg:gap-x-[5%]">
-        {products.map((product) => (
-          <ProductCard
-            key={product._id}
-            templateImg={product.image}
-            tags={product.tags}
-            name={product.templateName}
-            price={product.price}
-            category={product.category}
-          />
-        ))}
+      <div className="flex w-auto flex-col items-center px-8">
+        <div className="grid grid-cols-1 items-center justify-center gap-6 md:grid-cols-2 lg:w-full lg:max-w-screen-md lg:grid-cols-3">
+          {products.map((product) => (
+            <ProductCard
+              key={product._id}
+              templateImg={product.image}
+              tags={product.tags}
+              name={product.templateName}
+              price={product.price}
+              category={product.category}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
