492017186761c4579f12c70c7474d4bbb1a3416d
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-06-11 12:55:38 +0200

    console logs to debug deployed version

--- backend/controllers/museumController.js
+++ backend/controllers/museumController.js
@@ -17,15 +17,21 @@ export const getMuseumById = async (req, res) => {
   const museumId = req.params.id
 
   try {
+    console.log("Received request for museum ID:", museumId)
+
     if (!mongoose.Types.ObjectId.isValid(museumId)) {
       return res.status(400).json({ error: "Invalid museum ID" })
     }
 
-    const museum = await Museum.findById(museumId)
+    const museum = await Museum.findOne({ _id: museumId })
 
     if (!museum) {
+      console.log("Museum not found for ID:", museumId)
       return res.status(404).json({ error: "Museum not found" })
     }
+
+    console.log("Found museum for ID:", museumId)
+    console.log("Museum details:", museum)
 
     res.status(200).json(museum)
   } catch (error) {
