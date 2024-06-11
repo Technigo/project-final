a5b893fb2e4466cb9841c79792e00a6571cab81c
Frida Svensson <fridamarisvensson@gmail.com>
2024-06-11 16:35:50 +0200

    Add styling to not found page

--- frontend/src/pages/NotFoundPage/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button } from "../../common/ReusableComponents/Button/Button";
+import "./NotFoundPage.css";
 
 export const NotFoundPage = () => {
   return (
     <div className="not-found-page">
       <section className="not-found-container">
         <h2>(404) Page not found. </h2>
+        <p>Sorry, this page does not exist. Try going to a different page. </p>
         <Button to="/" variant="hero" label="Go to homepage" />
       </section>
     </div>
