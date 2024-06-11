68fd82b638d6a9670cfaafb8c85b65480f55a0ed
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-06-11 12:35:35 +0200

    endpoint for musuem id created

--- backend/routes/museumRoutes.js
+++ backend/routes/museumRoutes.js
@@ -1,15 +1,9 @@
 import express from "express"
-import { Museum } from "../models/Museum.js"
+import { getMuseums, getMuseumById } from "../controllers/museumController.js"
 
 const router = express.Router()
 
-router.get("/museums", async (req, res) => {
-  try {
-    const museums = await Museum.find()
-    res.status(200).json(museums)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
-})
+router.get("/museums", getMuseums)
+router.get("/museums/:id", getMuseumById)
 
 export default router
