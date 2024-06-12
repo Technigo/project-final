a93f80689b656d2f527f846fe21fea0168bca60d
Martin Jönsson <martin.t.joensson@gmail.com>
2024-06-12 12:19:32 +0200

    Changed the About Us text

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -8,17 +8,18 @@ export const AboutUs = () => {
     <>
       <div className="text-text-light w-3/4 tablet:max-w-screen-tablet m-auto">
         <h2 className="font-heading text-2xl text-center my-8">The idea</h2>
-        <p className="font-body text-center">
-          glim started with the idea that personalized products should be easier
-          to find. By entering your information in your profile you get
-          recommendations, allergy alerts and products that you know work with
-          you, not against you.
+        <p className="font-body text">
+          glim was founded on the principle that finding personalized products
+          should be simpler. By filling out your profile, you receive tailored
+          recommendations, allergy warnings, and products that are compatible
+          with your needs, ensuring they work for you, not against you.
         </p>
         <h2 className="font-heading text-2xl text-center my-8">About us</h2>
-        <p className="font-body text-center">
-          We are the ones that made this crazy website. It was made as a final
+        <p className="font-body">
+          We are the ones that made this crazy website. It was made during three weeks as a final
           project for the Technigo bootcamp.
         </p>
+        <p className="font-body my-4">If you are interested in getting in contact with us you van do so below.</p>
         <div className="flex m-10 justify-around">
           <div>
             <img
@@ -28,16 +29,27 @@ export const AboutUs = () => {
             />
             <h3 className="font-heading text-center text-xl my-2">Linda</h3>
             <div className="flex flex-col text-center">
-              <a className="hover:opacity-75 active:opacity-50
-               my-2" href="https://www.linkedin.com/in/lindafrischknecht/">
+              <a
+                className="hover:opacity-75 active:opacity-50
+               my-2"
+                href="https://www.linkedin.com/in/lindafrischknecht/"
+              >
                 Contact me
               </a>
-              <a className="hover:opacity-75 active:opacity-50
-              " href="https://linda-f-project-portfolio.netlify.app/">
+              <a
+                className="hover:opacity-75 active:opacity-50
+              "
+                href="https://linda-f-project-portfolio.netlify.app/"
+              >
                 Portfolio
               </a>
-              <a className="hover:opacity-75 active:opacity-50
-              " href="https://github.com/linda-f">GitHub</a>
+              <a
+                className="hover:opacity-75 active:opacity-50
+              "
+                href="https://github.com/linda-f"
+              >
+                GitHub
+              </a>
             </div>
           </div>
           <div>
@@ -48,14 +60,27 @@ export const AboutUs = () => {
             />
             <h3 className="font-heading text-center text-xl my-2">Izabel</h3>
             <div className="flex flex-col text-center">
-              <a className="hover:opacity-75 active:opacity-50
-               my-2" href="https://www.linkedin.com/in/izabel-lind-f%C3%A4rnstrand-6336071a2/">
+              <a
+                className="hover:opacity-75 active:opacity-50
+               my-2"
+                href="https://www.linkedin.com/in/izabel-lind-f%C3%A4rnstrand-6336071a2/"
+              >
                 Contact me
               </a>
-              <a className="hover:opacity-75 active:opacity-50
-              " href="https://izabellindfarnstrand.netlify.app/">Portfolio</a>
-              <a className="hover:opacity-75 active:opacity-50
-              " href="https://github.com/Izzibizz">GitHub</a>
+              <a
+                className="hover:opacity-75 active:opacity-50
+              "
+                href="https://izabellindfarnstrand.netlify.app/"
+              >
+                Portfolio
+              </a>
+              <a
+                className="hover:opacity-75 active:opacity-50
+              "
+                href="https://github.com/Izzibizz"
+              >
+                GitHub
+              </a>
             </div>
           </div>
           <div>
@@ -66,16 +91,27 @@ export const AboutUs = () => {
             />
             <h3 className="font-heading text-center text-xl my-2">Martin</h3>
             <div className="flex flex-col text-center">
-              <a className="hover:opacity-75 active:opacity-50
-               my-2" href="https://www.linkedin.com/in/martin-j%C3%B6nsson-058064163/">
+              <a
+                className="hover:opacity-75 active:opacity-50
+               my-2"
+                href="https://www.linkedin.com/in/martin-j%C3%B6nsson-058064163/"
+              >
                 Contact me
               </a>
-              <a className="hover:opacity-75 active:opacity-50
-              " href="https://martin-joensson-portfolio.netlify.app/">
+              <a
+                className="hover:opacity-75 active:opacity-50
+              "
+                href="https://martin-joensson-portfolio.netlify.app/"
+              >
                 Portfolio
               </a>
-              <a className="hover:opacity-75 active:opacity-50
-              " href="https://github.com/Martin-Joensson">GitHub</a>
+              <a
+                className="hover:opacity-75 active:opacity-50
+              "
+                href="https://github.com/Martin-Joensson"
+              >
+                GitHub
+              </a>
             </div>
           </div>
         </div>
