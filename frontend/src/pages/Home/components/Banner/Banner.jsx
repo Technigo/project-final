5075c8acdd98c48bfe495ce76f4f687967d28cd3
Frida Svensson <fridamarisvensson@gmail.com>
2024-06-12 22:27:41 +0200

    Create bubble-row

--- frontend/src/pages/Home/components/Banner/Banner.jsx
+++ frontend/src/pages/Home/components/Banner/Banner.jsx
@@ -1,26 +1,26 @@
 import { Image } from "../../../../common/ReusableComponents/Image/Image";
 import "./Banner.css";
-// import banner from "/assets/images/banner.png";
 import stonesAndCones from "/assets/images/stones-and-cones.svg";
 import skewedStamp from "/assets/logotypes/logo-stamp-skewed.svg";
 
 export const Banner = () => {
   return (
     <section className="banner-container">
-      {/* <Image
-        src={banner}
-        alt="image saying we believe in the power of play"
-        className="banner"
-      /> */}
+      <div className="bubble-row-top"></div>
       <div className="banner-content">
         <Image
           src={stonesAndCones}
           alt="Cones and stones"
           className="cones-and-stones"
         />
-        <h1>We belive <br className="first-br"></br>in the <br className="second-br"></br>power <br className="third-br"></br>of play.</h1>
+        <h1>
+          We belive <br className="first-br"></br>in the{" "}
+          <br className="second-br"></br>power <br className="third-br"></br>of
+          play.
+        </h1>
         <Image src={skewedStamp} alt="Stamp" className="stamp" />
       </div>
+      <div className="bubble-row-bottom"></div>
     </section>
   );
 };
