7f3b1ea2b77b87422ac9e3ec7c46cf94f73c5aac
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-06-12 15:55:54 +0200

    added active state to navbar links

--- frontend/src/components/styled/Footer.styled.jsx
+++ frontend/src/components/styled/Footer.styled.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 const StyledFooter = styled.footer`
-  background-color: #333333;
+  background-color: #222222;
   display: flex;
   flex-direction: column;
   width: 100%;
