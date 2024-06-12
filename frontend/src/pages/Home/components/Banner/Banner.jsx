0c144961375e1fa616d60ddd52b300a05a8390bf
Frida Svensson <fridamarisvensson@gmail.com>
2024-06-12 21:24:02 +0200

    Fix banner layout for alla screen sizes

--- frontend/src/pages/Home/components/Banner/Banner.jsx
+++ frontend/src/pages/Home/components/Banner/Banner.jsx
@@ -1,8 +1,8 @@
 import { Image } from "../../../../common/ReusableComponents/Image/Image";
 import "./Banner.css";
 // import banner from "/assets/images/banner.png";
-import stonesAndCones from "/assets/images/stones-and-cones.svg"
-import skewedStamp from "/assets/logotypes/logo-stamp-skewed.svg"
+import stonesAndCones from "/assets/images/stones-and-cones.svg";
+import skewedStamp from "/assets/logotypes/logo-stamp-skewed.svg";
 
 export const Banner = () => {
   return (
@@ -12,9 +12,15 @@ export const Banner = () => {
         alt="image saying we believe in the power of play"
         className="banner"
       /> */}
-      <h1>We belive in the power of play.</h1>
-      <Image src={stonesAndCones}/>
-      <Image src={skewedStamp}/>
+      <div className="banner-content">
+        <Image
+          src={stonesAndCones}
+          alt="Cones and stones"
+          className="cones-and-stones"
+        />
+        <h1>We belive <br className="first-br"></br>in the <br className="second-br"></br>power <br className="third-br"></br>of play.</h1>
+        <Image src={skewedStamp} alt="Stamp" className="stamp" />
+      </div>
     </section>
   );
 };
