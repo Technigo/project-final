f0355b56eeedd73972c9c22767d5bb09545a3567
Frida Svensson <fridamarisvensson@gmail.com>
2024-06-12 13:52:40 +0200

    Put h1 in hero-text wrapper and update some styling so that it looks better on all screens

--- frontend/src/pages/Home/components/Hero/Hero.jsx
+++ frontend/src/pages/Home/components/Hero/Hero.jsx
@@ -11,8 +11,8 @@ export const Hero = () => {
         alt="Hero-image of small girl dancing in the field"
         className="hero"
       />
-      <h1 className="hero-h1">Summer vibes</h1>
       <div className="hero-text">
+        <h1 className="hero-h1">Summer vibes</h1>
         <h2 className="hero-h4">Playful clothes for sunny days</h2>
         <Button variant="shop" label="See all products" to="/products" />
       </div>
