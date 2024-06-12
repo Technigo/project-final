984eae6d39615484d03e19f2f3189e0a0caa8d79
Frida Svensson <fridamarisvensson@gmail.com>
2024-06-12 15:31:48 +0200

    Create separate elements in banner instead of having a fixed image

--- frontend/src/pages/Home/components/Banner/Banner.jsx
+++ frontend/src/pages/Home/components/Banner/Banner.jsx
@@ -1,15 +1,20 @@
 import { Image } from "../../../../common/ReusableComponents/Image/Image";
 import "./Banner.css";
-import banner from "/assets/images/banner.png";
+// import banner from "/assets/images/banner.png";
+import stonesAndCones from "/assets/images/stones-and-cones.svg"
+import skewedStamp from "/assets/logotypes/logo-stamp-skewed.svg"
 
 export const Banner = () => {
   return (
     <section className="banner-container">
-      <Image
+      {/* <Image
         src={banner}
         alt="image saying we believe in the power of play"
         className="banner"
-      />
+      /> */}
+      <h1>We belive in the power of play.</h1>
+      <Image src={stonesAndCones}/>
+      <Image src={skewedStamp}/>
     </section>
   );
 };
