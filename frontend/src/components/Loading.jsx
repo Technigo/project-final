01f47cf1599625ea3e6804d87e62f18632642438
SofieFerrari <sofiestrahl@gmail.com>
2024-06-13 08:02:32 +0200

    added styling to lottie

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -3,8 +3,12 @@ import loading from "../assets/animation/lottie-loading.json";
 
 export const Loading = () => {
   return (
-    <div>
-      <Lottie animationData={loading} loop={true} />
+    <div className="flex justify-center">
+      <Lottie
+        animationData={loading}
+        loop={true}
+        style={{ width: 300, height: 300 }}
+      />
     </div>
   );
 };
