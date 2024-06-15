8ff841068bfd16bfc71899b6ed22f4a57d79f582
unknown <linda.frischknecht@bluewin.ch>
2024-06-15 09:25:56 +0200

    adding cors thing to backend

--- backend/app.js
+++ backend/app.js
@@ -6,19 +6,19 @@ import seedDatabase from "./config/seeddatabase";
 import documentationRoutes from "./routes/documentation";
 import productsRoutes from "./routes/products";
 import reviewRoutes from "./routes/reviews";
-import stripeRoutes from "./routes/stripe"
+import stripeRoutes from "./routes/stripe";
 import userRoutes from "./routes/users";
 
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
 //TODO Add CORS Link
-app.use(cors());
+app.use(cors("https://glim-skincare.netlify.app/"));
 app.use(express.json());
 app.use("/", documentationRoutes);
 app.use("/products", productsRoutes);
 app.use("/users", userRoutes);
 app.use("/reviews", reviewRoutes);
-app.use("/stripe", stripeRoutes)
+app.use("/stripe", stripeRoutes);
 
 export default app;
