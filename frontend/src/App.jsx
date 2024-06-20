9d2e5058b1594d3d5b86526474dd7f5536c2c6a5
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-06-20 16:00:22 +0200

    Implement scroll to top function when navigating

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { ScrollToTop } from "./components/ScrollToTop";
 import { AppRoutes } from "./routes/AppRoutes";
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <AppRoutes />
       <Footer />
